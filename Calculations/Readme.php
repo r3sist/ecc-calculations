cb188e107fccdaa93393fa7e33fe34312217a872
resist <vankos@resist.hu>
2018-06-26 09:37:12 +0200

    a README from repositories to Readme doc

--- Calculations/Readme.php
+++ Calculations/Readme.php
@@ -7,6 +7,25 @@ Class Readme extends \Ecc
 
     public function calc($f3)
     {
-        
+        $blc = \Blc::instance();
+
+        $text0 = '
+A dokumentum a [structure](https://bitbucket.org/resist/source-structure) és az 
+[ecc-calculations](https://bitbucket.org/resist/ecc-calculations) repository-k README.md fájlját jeleníti meg. 
+A számítások changelogja a [ecc-calculations commitjainál](https://bitbucket.org/resist/ecc-calculations/commits) követhetők.
+
+---
+';
+
+        $blc->md($text0);
+
+        $blc->hr();
+        $blc->md('[ecc-calculations]()');
+
+        $text1 = file_get_contents($f3->BASE.'vendor/resist/ecc-calculations/README.md');
+        $blc->md($text1);
+
+        $text2 = file_get_contents($f3->BASE.'README.md');
+        $blc->md($text2);
     }
 }
