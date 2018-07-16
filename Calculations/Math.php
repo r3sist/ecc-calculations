5bd9302511654dd155934c547c88f2f6f2df51b4
resist <vankos@resist.hu>
2018-07-16 13:05:20 +0200

    Math: fix temperature calculation

--- Calculations/Math.php
+++ Calculations/Math.php
@@ -18,17 +18,16 @@ Class Math extends \Ecc
         $blc->def('slope_deg', number_format($slope_per, 2), $f3->_slope.'° = %% %', '');
 
         $blc->h1('Hőmérséklet rudakon');
-        $blc->def('alpha_T_st', 0.000012, 'alpha_(T,steel) = %% 1/K', '');
+        $blc->def('alpha_T_st', number_format(0.000012, 6), 'alpha_(T,steel) = %% 1/K', '');
         $blc->input('L', 'Rúdhossz', '10', 'm', '');
         $blc->input('DeltaT', 'Hőmérséklet változás', '40', '°', '');
-        $blc->def('DeltaL', $f3->_alpha_T_st*$f3->_L*$f3->_deltaT*1000, 'DeltaL = %% mm', '');
+        $blc->def('DeltaL', number_format($f3->_alpha_T_st*($f3->_L*1000)*$f3->_DeltaT, 2), 'DeltaL = %% mm', '');
 
         $blc->h1('Lineáris interpoláció');
         $blc->input('x1', '', '1', '', '');
         $blc->input('y1', '', '5', '', '');
         $blc->input('x2', '', '2', '', '');
         $blc->input('y2', '', '20', '', '');
-
         $blc->info0('i0');
         $blc->input('x', '', '3', '', '');
         $blc->def('y', (($f3->_x - $f3->_x1)*($f3->_y2 - $f3->_y1)/($f3->_x2 - $f3->_x1)) + $f3->_y1, 'y = %%');
