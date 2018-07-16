2b93faa96ac537cddbc5addb104ab9bd39c1f20b
resist <vankos@resist.hu>
2018-07-16 17:31:57 +0200

    Pin: dev

--- Calculations/Pin.php
+++ Calculations/Pin.php
@@ -10,116 +10,90 @@ Class Pin extends \Ecc
         $ec = \Ec::instance();
         $blc = \Blc::instance();
 
-        $ec->boltList('btName');
-        $ec->matList('btMat', '8.8', 'Csavar anyag');
-        $ec->matList('stMat', 'S235', 'Lemez anyag');
-        $blc->input('t', 'Kisebbik lemez vastagság', 10, 'mm', '');
-        $blc->input('n', 'Nyírási síkok száma', 1, '', '');
-        $blc->input('N', '`F_(t.Ed)` Húzóerő', 20, 'kN', '');
-        $blc->input('V', '`F_(v.Ed)` Nyíróerő', 30, 'kN', '');
+        $blc->h1('Saru erők', '');
+        $blc->input('REdz', '`R_(Ed,z)` Függőleges reakcióerő', 950, 'kN', '');
+        $blc->input('REdx', '`R_(Ed,x)` Hídtengely irányú vízszintes reakcióerő', 10, 'kN', '');
+        $blc->input('REdy', '`R_(Ed,y)` Kereszt irányú vízszintes reakcióerő', 90, 'kN', '');
+        $blc->input('REdzser', '`R_(Ed,z,ser)` Függőleges reakcióerő használhatósági határállapotban', 460, 'kN', '');
+        $blc->input('REdxser', '`R_(Ed,x,ser)` Hídtengely irányú vízszintes reakcióerő használhatósági határállapotban', 200, 'kN', '');
+        $blc->input('lamda', 'Dinamikus tényező', 1.2, '', '');
+        $blc->def('REd', $f3->_lamda*sqrt(pow($f3->_REdz, 2) + pow($f3->_REdx, 2)), 'R_(Ed) = lamda*sqrt(R_(Ed,z)^2 + R_(Ed,x)^2) = %% [kN]');
+        $blc->def('REdser', sqrt(pow($f3->_REdzser, 2) + pow($f3->_REdxser, 2)), 'R_(Ed,ser) = lamda*sqrt(R_(Ed,z,ser)^2 + R_(Ed,x,ser)^2) = %% [kN]');
 
-        $blc->region0('r0', 'Jellemzők');
-            $blc->math('btMat = '.$f3->_btMat);
-            $blc->def('f_yb', $ec->matProp($f3->_btMat, 'fy'),'f_(y,b) = %% [MPa]', 'Csavar folyáshatár');
-            $blc->def('f_ub', $ec->matProp($f3->_btMat, 'fu'),'f_(u,b) = %% [MPa]', 'Csavar szakítószilárdság');
-            $blc->def('d_0', $ec->boltProp($f3->_btName, 'd0'),'d_0 = %% [mm]', 'Lyuk átmérő');
-            $blc->def('A', $ec->boltProp($f3->_btName, 'A'),'A = %% [mm^2]', 'Csavar keresztmetszeti terület');
-            $blc->def('A_s', $ec->boltProp($f3->_btName, 'As'),'A_s = %% [mm^2]', 'Csavar húzási keresztmetszet');
-            $blc->math('stMat = '.$f3->_stMat);
-            $blc->def('f_u', $ec->fu($f3->_stMat, $f3->_t),'f_u = %% [MPa]', 'Lemez szakítószilárdság');
-        $blc->region1('r0');
+        $blc->h1('Csap', 'Csavar osztály: ***A*** - tengelyre merőlegesen terhelt, nem feszített');
+        $ec->matList('btMat', '6.8', 'Csap/csavar anyag');
+        $blc->def('fyb', $ec->matProp($f3->_btMat, 'fy'), 'f_(y,b) = %% [MPa]', 'Csavar folyáshatár');
+        $blc->def('fub', $ec->matProp($f3->_btMat, 'fu'), 'f_(u,b) = %% [MPa]', 'Csavar szakítószilárdság');
+        $blc->success0('s0');
+            $blc->def('d_y', ceil(sqrt((4*$f3->_REd*1000)/(pi()*$f3->_fyb))), 'd_y = sqrt((4*R_(Ed))/(pi*f_(y,b))) = %% [mm]', 'Csavar folyáshoz számított javasolt átmérő');
+        $blc->success1('s0');
+        $blc->input('d', 'Csap átmérő', 70, 'mm', '');
+        $blc->def('d0', $f3->_d + 3, 'd_0 = d + 3 = %% [mm]', 'Figyelembe vett lukméret');
+        $blc->input('n', 'Nyírási síkok száma', 2, '', '');
+        $blc->def('e1', 2*$f3->_d0, 'e_1 = 2*d_0 = %% [mm]', 'Figyelembe vett peremtávolság');
+        $blc->def('e2', 2*$f3->_d0, 'e_2 = 2*d_0 = %% [mm]', 'Figyelembe vett peremtávolság');
+        $blc->def('A_b', (pow($f3->_d, 2)*pi())/4, 'A_b = (d^2*pi)/4 = %% [mm^2]', 'Figyelembe vett csap keresztmetszeti terület');
 
-        $blc->region0('r1', 'Csavar adatbázis');
-            $blc->table($ec->boltDb(), 'Csavar','');
-        $blc->region1('r1');
+        $blc->h1('Sarulemezek', '');
+        $blc->input('t1', 'Szélső talp lemezvastagság', 25, 'mm', '');
+        $blc->input('t2', 'Belső hídlemez lemezvastagság', 40, 'mm', '');
+        $blc->def('c', 2, 'c = 2 [mm]', 'Lemezek közti hézag');
+        $ec->matList('stMat', 'S355', 'Lemez anyagminőség');
+        $blc->def('fy', $ec->fy($f3->_stMat, max($f3->_t1, $f3->_t2)), 'f_y = %% [MPa]', 'Lemez folyáshatár');
+        $blc->def('fu', $ec->fu($f3->_stMat, max($f3->_t1, $f3->_t2)), 'f_u = %% [MPa]', 'Lemez szakítószilárdság');
 
-        $blc->h1('Egy csavar húzási- és kigombolódási ellenállása', '***D*** nem feszített, húzott és ***E*** feszített, húzott csavarok');
-        $blc->def('F_tRd', $ec->FtRd($f3->_btName, $f3->_btMat),'F_(t,Rd) = %% [kN]', 'Csavar húzási ellenállása');
-        $blc->label($f3->_N/$f3->_F_tRd, 'Húzási kihasználtság');
-        $blc->def('B_pRd', $ec->BpRd($f3->_btName, $f3->_stMat, $f3->_t),'B_(p,Rd) = %% [kN]', 'Csavar kigombolódási ellenállása');
-        $blc->label($f3->_N/$f3->_B_pRd, 'Kigombolódási kihasználtság');
+        $write = array(
+            array('size' => 10, 'x' => 90, 'y' => 360, 'text' => 't1='.$f3->_t1.''),
+            array('size' => 10, 'x' => 255, 'y' => 360, 'text' => 't1='.$f3->_t1.''),
+            array('size' => 10, 'x' => 175, 'y' => 360, 'text' => 't2='.$f3->_t2.''),
+            array('size' => 10, 'x' => 320, 'y' => 245, 'text' => 'd='.$f3->_d.''),
+            array('size' => 10, 'x' => 25, 'y' => 245, 'text' => 'd0='.$f3->_d0.''),
 
-        $blc->h1('Egy csavar nyírási- és palástnyomási ellenállása', '***A*** osztály: nem feszített, nyírt csavar');
-        $blc->boo('inner', 'Belső csavar', 1, '');
-        $blc->input('e1', 'Peremtávolság (csavarképpel párhuzamos)', 50, 'mm', '');
-        $blc->input('e2', 'Peremtávolság (csavarképre merőleges)', 50, 'mm', '');
-        $blc->def('F_vRd', $ec->FvRd($f3->_btName, $f3->_btMat, $f3->_n),'F_(v,Rd) = %% [kN]', 'Csavar nyírási ellenállása');
-        $blc->label($f3->_V/$f3->_F_vRd, 'Nyírási kihasználtság');
-        $blc->def('F_bRd', $ec->FbRd($f3->_btName, $f3->_btMat, $f3->_stMat, $f3->_e1, $f3->_e2, $f3->_t, $f3->_inner),'F_(b,Rd) = %% [kN]', 'Csavar palástnyomási ellenállása');
-        $blc->label($f3->_V/$f3->_F_bRd, 'Palástnyomási kihasználtság');
-
-        $blc->region0('r2', '*k.1* és *&alpha;.b* tényezők');
-            $blc->math('k_1 = '.$f3->___k1.'%%%alpha_b = '.$f3->___alphab);
-        $blc->region1('r2');
-
-        $blc->h1('Egy csavar húzás és nyírás interakciója', '***AD*** osztály');
-        $blc->def('U_vt', (($f3->_V / $f3->_F_vRd) + ($f3->_N / (1.4*$f3->_F_tRd)))*100, 'U_(vt) = F_(v,Ed)/F_(v,Rd) + F_(t,Ed)/(1.4*F_(t,Rd)) = %% [%]', 'Interakciós kihasználtság');
-        $blc->label($f3->_U_vt/100, 'Interakciós kihasználtság');
-
-        $blc->h1('Egyszerűsített eljárás nyírásra optimalizálásra');
-        $deltaDb = array(
-            '400' => array(
-                '360' => 3.18
-            ),
-            '500' => array(
-                '360' => 2.53,
-                '430' => 3.04
-            ),
-            '600' => array(
-                '360' => 2.12,
-                '430' => 2.54,
-                '510' => 3.01,
-                '530' => 3.13
-            ),
-            '800' => array(
-                '360' => 1.59,
-                '430' => 1.90,
-                '510' => 2.26,
-                '530' => 2.34
-            ),
-            '1000' => array(
-                '360' => 1.27,
-                '430' => 1.52,
-                '510' => 1.80,
-                '530' => 1.82
-            )
         );
-        $delta = $deltaDb[$ec->matProp($f3->_btMat, 'fu')][$ec->matProp($f3->_stMat, 'fu')];
-        $blc->def('delta', $delta, 'delta = %%', '*[Általános eljárások 6.3. táblázat]*');
-        $blc->info0('r3');
-            $blc->def('d_min', number_format($f3->_delta * $f3->_t, 0),'d_(min) = delta*t = %% [mm]');
-            $blc->def('e1_opt', number_format(2*$f3->_d_0, 0),'e_(1,opt) = 2*d_0 = %% [mm]', 'Szélső peremtávolság (csavarképpel párhuzamos)');
-            $blc->def('e2_opt', number_format(1.5*$f3->_d_0, 0),'e_(2,opt) = 1.5*d_0 = %% [mm]', 'Szélső peremtávolság (csavarképre merőleges)');
-            $blc->def('p1_opt', number_format(3*$f3->_d_0, 0),'p_(1,opt) = 3*d_0 = %% [mm]', 'Belső csvaar távolság (csavarképpel párhuzamos)');
-            $blc->def('p2_opt', number_format(3*$f3->_d_0, 0),'p_(2,opt) = 3*d_0 = %% [mm]', 'Belső csvavar távolság (csavarképre merőleges)');
-        $blc->info1('r3');
+        $blc->write('vendor/resist/ecc-calculations/canvas/pin0.jpg', $write, 'Saru elrendezés');
 
-        $blc->h2('Feszített csavarok nyírásra');
-        if ($f3->_btMat != '10.9') {
-            $blc->label('no', 'Nem 10.9 csavar');
-        }
-        $blc->input('n_s', 'Súrlódó felületek száma', 1, '', '');
-        $blc->input('mu', 'Súrlódási tényező', 0.2, '', '**Súrlódási tényezők:** **0.5** *Sörétezett vagy szemcsefújt festetlen felület*, **0.4** *Sörétezett vagy szemcsefújt festett felület*, **0.3** *Drótkefézett vagy lángszórással tisztított felület*, **0.2** *Kezeletlen felület*');
-        $blc->input('F_Ed_ser', '`F_(Ed,ser):` Nyíróerő használhatósági határállapotban', 10, 'kN', '');
+        $blc->h1('Nyírt csavar ellenállása', '');
+        $blc->def('FVRd', $ec->FvRd('M12', $f3->_btMat, $f3->_n, $f3->_A_b), 'F_(V,Rd) = %% [kN]', 'Csavarként vett csap nyírási ellenállása');
 
-        $blc->md('***B*** osztályú nyírt csavarok használhatósági határállapotig működnek feszített csavarként.');
-        $blc->md('Teherbírási határállapotban ***A*** csavarként. Használhatósági határállapotban:');
+        $blc->h1('Nyírt rugalmas csap ellenállása', '');
+        $blc->def('FVRdel', ($f3->_n*0.6*$f3->_A_b*$f3->_fyb)/($f3->__GM2*1000), 'F_(V,Rd, el) = (n*0.6*A_b*f_(y,b))/gamma_(M2) = %% [kN]', 'Csap rugalmas nyírási ellenállása');
 
-        $blc->def('F_pC', 0.7*$f3->_f_ub*$f3->_A_s/1000,'F_(p,C) = 0.7*f_(u,b)*A_s = %% [kN]', 'Előírt feszítőerő');
-        $blc->def('F_s_Rd', (($f3->_n_s*$f3->_mu)/$f3->__GM3)*$f3->_F_pC,'F_(s,Rd) = (n_s*mu)/gamma_(M3)*F_(p,C) = %% [kN]', 'Megcsúszással szembeni ellenállás');
-        $f3->_U_s_ser = $f3->_F_Ed_ser/$f3->_F_s_Rd;
-        $blc->label($f3->_U_s_ser, 'Kihasználtság használhatósági határállapotban');
+        $blc->h1('Csavar palástnyomási ellenállása', '');
+        $k1 = min(2.8*($f3->_e2/ $f3->_d0) - 1.7, 2.5);
+        $alphab = min(($f3->_e1/ (3* $f3->_d0)), $f3->_fub/ $f3->_fu, 1);
+        $FbRd1 = $k1*(($alphab*$f3->_fu* $f3->_d * $f3->_t1)/(1000 * $f3->__GM2));
+        $FbRd2 = $k1*(($alphab*$f3->_fu* $f3->_d * $f3->_t2)/(1000 * $f3->__GM2));
+        $blc->def('FbRd1', $FbRd1, 'F_(b,Rd,t1) = %% [kN]', 'Csavarként vett csap palástnyomási ellenállása, \`t1\` lemezhez');
+        $blc->def('FbRd2', $FbRd2, 'F_(b,Rd,t2) = %% [kN]', 'Csavarként vett csap palástnyomási ellenállása, \`t2\` lemezhez');
 
-        $blc->md('***C*** osztályú nyírt csavar:');
-        $f3->_U_s = $f3->_V/$f3->_F_s_Rd;
-        $blc->label($f3->_U_s, 'Kihasználtság teherbírási határállpotban');
-        $blc->label($f3->_V/$f3->_F_bRd, 'Palástnyomási kihasználtság');
+        $blc->h1('Rugalmas csap palástnyomási ellenállása', '');
+        $FbRdel1 = (1.5*$f3->_fy*$f3->_d*$f3->_t1)/($f3->__GM2*1000);
+        $FbRdel2 = (1.5*$f3->_fy*$f3->_d*$f3->_t2)/($f3->__GM2*1000);
+        $blc->def('FbRdel1', $FbRdel1, 'F_(b,Rd,el,t1) = (1.5*f_y*d*t_1)/gamma_(M2) = %% [kN]', 'Csap palástnyomási ellenállása, \`t1\` lemezhez');
+        $blc->def('FbRdel2', $FbRdel2, 'F_(b,Rd,el,t2) = (1.5*f_y*d*t_2)/gamma_(M2) = %% [kN]', 'Csap palástnyomási ellenállása, \`t2\` lemezhez');
 
-        $blc->md('***CE*** osztályú húzott-nyírt csavar:');
-        $blc->def('F_s_tv_Rd', (($f3->_n_s*$f3->_mu)/$f3->__GM3)*($f3->_F_pC-0.8*$f3->_N),'F_(s,tv,Rd) = (n_s*mu)/gamma_(M3)*(F_(p,C)-0.8*F_(t,Ed)) = %% [kN]', 'Interakciós ellenállás');
-        $f3->_U_s_tv = $f3->_V/$f3->_F_s_tv_Rd;
-        $blc->label($f3->_U_s_tv, 'Interakciós kihasználtság');
+        $blc->h1('Cserélhető csap használhatósági feltétele nyírásból', 'Csak tervezetten cserélhető csapok esetén releváns');
+        $FbRdser1 = (0.6*$f3->_d*$f3->_t1*$f3->_fy)/($f3->__GM6ser*1000);
+        $FbRdser2 = (0.6*$f3->_d*$f3->_t2*$f3->_fy)/($f3->__GM6ser*1000);
+        $blc->def('FbRdser1', $FbRdser1, 'F_(b,Rd,ser,t1) = (0.6*f_y*d*t_1)/gamma_(M6,ser) = %% [kN]', 'Használati határteherbírás, \`t1\` lemezhez');
+        $blc->def('FbRdser2', $FbRdser2, 'F_(b,Rd,ser,t2) = (0.6*f_y*d*t_2)/gamma_(M6,ser) = %% [kN]', 'Használati határteherbírás, \`t2\` lemezhez');
 
-        $blc->h2('Csoportos kiszakadás');
+        $blc->h1('Csap hajlítási ellenállása', '');
+        $blc->def('MbEd', ($f3->_REd/8)*($f3->_t1*2 + $f3->_t2 + 2*$f3->_c)/1000, 'M_(b,Ed) = R_(Ed)/8*(2*t_1 + t_2 + 2*c) = %% [kNm]');
+        $blc->def('Wel', 0.1*pow($f3->_d, 3), 'W_(el) = 0.1*d^3 = %% [mm^3]');
+        $blc->def('MbRdel', (1.5*$f3->_Wel*$f3->_fyb)/($f3->__GM0*1000000), 'M_(b,Rd,el) = (1.5*W_(el)*f_(y,b))/gamma_(M0) = %% [kNm]');
+
+        $blc->h1('Cserélhető csap használhatósági feltétele hajlításból', 'Csak tervezetten cserélhető csapok esetén releváns');
+        $blc->def('MbEdser', ($f3->_REdser/8)*($f3->_t1*2 + $f3->_t2 + 2*$f3->_c)/1000, 'M_(b,Ed,ser) = R_(Ed)/8*(2*t_1 + t_2 + 2*c) = %% [kNm]');
+        $blc->def('MbRdser', (0.8*$f3->_Wel*$f3->_fyb)/($f3->__GM6ser*1000000), 'M_(b,Rd,ser) = (0.8*W_(el)*f_(y,b))/gamma_(M6,ser) = %% [kNm]');
+
+        $blc->h1('Nyírás és hajlítás interakciója');
+        $blc->def('U70', pow($f3->_MbEd/$f3->_MbRdel, 2) + pow($f3->_REd/$f3->_FVRdel, 2), '%%');
+
+        $blc->h1('Egyszerűsített varratellenőrzések');
+
+        $blc->h1('Szerkesztési szabályok');
+
+        $blc->h1('Bebetonozott tőcsavarok ellenőrzése');
     }
 }
