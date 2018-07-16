555e5bfa5dab2e498ad850e5b2020306198908ae
resist <vankos@resist.hu>
2018-07-16 13:00:25 +0200

    Math: add linear interpolation

--- Calculations/Math.php
+++ Calculations/Math.php
@@ -22,5 +22,16 @@ Class Math extends \Ecc
         $blc->input('L', 'Rúdhossz', '10', 'm', '');
         $blc->input('DeltaT', 'Hőmérséklet változás', '40', '°', '');
         $blc->def('DeltaL', $f3->_alpha_T_st*$f3->_L*$f3->_deltaT*1000, 'DeltaL = %% mm', '');
+
+        $blc->h1('Lineáris interpoláció');
+        $blc->input('x1', '', '1', '', '');
+        $blc->input('y1', '', '5', '', '');
+        $blc->input('x2', '', '2', '', '');
+        $blc->input('y2', '', '20', '', '');
+
+        $blc->info0('i0');
+        $blc->input('x', '', '3', '', '');
+        $blc->def('y', (($f3->_x - $f3->_x1)*($f3->_y2 - $f3->_y1)/($f3->_x2 - $f3->_x1)) + $f3->_y1, 'y = %%');
+        $blc->info1('i0');
     }
 }
