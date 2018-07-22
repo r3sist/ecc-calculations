c6c197c46552a8c95157687f06e04df932a0380f
resist <vankos@resist.hu>
2018-07-22 14:26:46 +0200

    SteelSection: impr.

--- Calculations/SteelSection.php
+++ Calculations/SteelSection.php
@@ -43,17 +43,23 @@ Class SteelSection extends \Ecc
         $blc->h1('Nyírt keresztmetszet');
         $blc->note('[Szürke 2007: 5.1.5 41.o]');
         $blc->success0('v');
-        $blc->def('VplRd', $ec->VplRd($f3->_A_v, $f3->_mat, $f3->_t), 'V_(c,Rd) = V_(pl,Rd) = (A_v*f_y)/(sqrt(3)*gamma_(M0)) = %% kN', 'Nyírási ellenállás 1 és 2. kmo. esetén');
+        $blc->def('VplRd', $ec->VplRd($f3->_A_v, $f3->_mat, $f3->_t), 'V_(c,Rd) = V_(pl,Rd) = (A_v*f_y)/(sqrt(3)*gamma_(M0)) = %% [kN]', 'Nyírási ellenállás 1 és 2. kmo. esetén');
         $blc->success1('v');
         $blc->note('3 és 4. kmo. esetén `V_(c,Rd) = (A_w*f_y)/(sqrt(3)*gamma_(M0))` H és I szelvénynél. Általános esetben `(I*t)/S*f_y/(sqrt(3)*gamma_(M0))`');
+
+        $blc->input('VEd', '`V_(Ed)` nyíróerő', 100, 'kN');
+        $blc->label($f3->_VEd/$f3->_VplRd, 'Nyírási kihasználtság');
 
         $blc->h1('Központosan húzott keresztmetszet');
         $blc->note('[Szürke 2007: 5.1.2 40.o]');
         $blc->note('Keresztmetszeti besorolásra nincs szükség.');
-        $blc->def('NplRd', $ec->NplRd($f3->_A, $f3->_mat, $f3->_t), 'N_(pl,Rd) = (A*f_y)/gamma_(M,0) = %% kN', 'Teljes km. folyási ellenállása');
-        $blc->def('NuRd', $ec->NuRd($f3->_A_net, $f3->_mat, $f3->_t), 'N_(u,Rd) = (0.9*A_(n\et)*f_u)/gamma_(M,2) = %% kN', 'Nettó km. képlékeny töréssel szembeni ellenállása');
+        $blc->def('NplRd', $ec->NplRd($f3->_A, $f3->_mat, $f3->_t), 'N_(pl,Rd) = (A*f_y)/gamma_(M,0) = %% [kN]', 'Teljes km. folyási ellenállása');
+        $blc->def('NuRd', $ec->NuRd($f3->_A_net, $f3->_mat, $f3->_t), 'N_(u,Rd) = (0.9*A_(n\et)*f_u)/gamma_(M,2) = %% [kN]', 'Nettó km. képlékeny töréssel szembeni ellenállása');
         $blc->success0('t');
-        $blc->def('NcRd', $ec->NtRd($f3->_A, $f3->_A_net, $f3->_mat, $f3->_t), 'N_(t,Rd) = min(N_(pl,Rd), N_(u,Rd)) = %% kN', 'Húzási ellenállás');
+        $blc->def('NtRd', $ec->NtRd($f3->_A, $f3->_A_net, $f3->_mat, $f3->_t), 'N_(t,Rd) = min(N_(pl,Rd), N_(u,Rd)) = %% [kN]', 'Húzási ellenállás');
         $blc->success1('t');
+
+        $blc->input('NEd', '`N_(Ed)` húzóerő', 200, 'kN');
+        $blc->label($f3->_NEd/$f3->_NtRd, 'Húzási kihasználtság');
     }
 }
