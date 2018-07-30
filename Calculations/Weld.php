f8ff35eedc718c5bd6286732fa59295be76fa1ed
resist <vankos@resist.hu>
2018-07-30 16:13:11 +0200

    Weld: add weld length checks

--- Calculations/Weld.php
+++ Calculations/Weld.php
@@ -19,21 +19,41 @@ Class Weld extends \Ecc
         $blc->def('w', $f3->_w + 1, 'w_(sarok) = %%');
 
         $blc->region0('r0');
-        $blc->math('mat = '.$f3->_mat);
-        $blc->def('l',$f3->_L - 2*$f3->_a,'l = L - 2*a = %% [mm]', 'Figyelembe vett varrathossz');
-        $blc->def('bw',$ec->matProp($f3->_mat, 'betaw'),'beta_w = %%', 'Hegesztési tényező');
-        $blc->def('fy',$ec->fy($f3->_mat, $f3->_t),'f_y=%% [MPa]', 'Folyáshatár');
-        $blc->def('fu',$ec->fu($f3->_mat, $f3->_t),'f_u=%% [MPa]', 'Szakítószilárdság');
-        $blc->def('FwEd',$f3->_F / $f3->_l *1000,'F_(w,Ed) = F_(Ed)/l = %% [(kN)/m]', 'Fajlagos igénybevétel');
+        $blc->math('mat = ' . $f3->_mat);
+        $blc->def('l', $f3->_L - 2 * $f3->_a, 'l = L - 2*a = %% [mm]', 'Figyelembe vett varrathossz');
+        $blc->def('bw', $ec->matProp($f3->_mat, 'betaw'), 'beta_w = %%', 'Hegesztési tényező');
+        $blc->def('fy', $ec->fy($f3->_mat, $f3->_t), 'f_y=%% [MPa]', 'Folyáshatár');
+        $blc->def('fu', $ec->fu($f3->_mat, $f3->_t), 'f_u=%% [MPa]', 'Szakítószilárdság');
+        $blc->def('FwEd', $f3->_F / $f3->_l * 1000, 'F_(w,Ed) = F_(Ed)/l = %% [(kN)/m]', 'Fajlagos igénybevétel');
         $blc->region1('r0');
 
+        if ($f3->_l <= 30) {
+            $blc->danger('Varrathossz rövidebb 30 mm-nél! Szabvány szerint nem figyelembe vehető.');
+        }
+
+        if ($f3->_l <= 6 * $f3->_a) {
+            $blc->danger('Varrathossz rövidebb \`6*a = ' . 6 * $f3->_a . '[mm]\`-nél! Szabvány szerint nem figyelembe vehető.');
+        }
+
+        if (150 * $f3->_a < $f3->_l) {
+            $blc->danger('\`l >= 150*a = ' . 150 * $f3->_a . ' [mm]\`, ezért indokolt a varrat teherbírásának csökkentése, nem zártszelvények esetén.');
+        }
+
         $blc->success0('s0');
-        $blc->def('FwRd',($f3->_fu*$f3->_a)/(sqrt(3)*$f3->_bw*$f3->__GM2)*($f3->_w),'F_(w,Rd) = (f_u*a)/(sqrt(3)*beta_w*gamma_(M2))*w_(sarok)= %% [(kN)/m]', 'Fajlagos teherbírás');
-        $blc->def('FwRdS',($f3->_FwRd*$f3->_l/1000),'F_(w,Rd,sum) = F_(w,Rd)*l = %% [kN]', 'Varratkép teljes teherbírása:');
-        $blc->label($f3->_F/$f3->_FwRdS, 'Kihasználtság');
+        $blc->def('FwRd', ($f3->_fu * $f3->_a) / (sqrt(3) * $f3->_bw * $f3->__GM2) * ($f3->_w), 'F_(w,Rd) = (f_u*a)/(sqrt(3)*beta_w*gamma_(M2))*w_(sarok)= %% [(kN)/m]', 'Fajlagos teherbírás');
+        $blc->def('FwRdS', ($f3->_FwRd * $f3->_l / 1000), 'F_(w,Rd,sum) = F_(w,Rd)*l = %% [kN]', 'Varratkép teljes teherbírása:');
+        $blc->label($f3->_F / $f3->_FwRdS, 'Kihasználtság');
         $blc->success1('s0');
 
         $blc->hr();
         $blc->img('https://structure.hu/ecc/weld0.jpg', 'CÉH Tekla hegesztési utasítás');
+        $blc->hr();
+
+        $blc->h1('Feszültség alapú általános eljárás');
+        $blc->input('sigma_T', 'Merőleges normálfeszültség', 100, 'MPa');
+        $blc->input('tao_T', 'Merőleges nyírófeszültség', 100, 'MPa');
+        $blc->input('tao_ii', '`tao_(||)`: Párhuzamos nyírófeszültség', 100, 'MPa');
+        $blc->txt('Megfelelőségi feltételek:');
+        $blc->math('sqrt(sigma_T^2 + 3*(tao_(ii)^2 + tao_T^2)) < f_u/(beta_w*gamma_(M2))');
     }
 }
