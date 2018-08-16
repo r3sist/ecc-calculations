f9de3dfdaea4caa9b5b1d5a9c01ebcf3a4518f50
resist <vankos@resist.hu>
2018-08-16 12:27:11 +0200

    Kata: remove BS grid; disable footer; add Teljesítésigazolás

--- Calculations/Kata.php
+++ Calculations/Kata.php
@@ -38,262 +38,214 @@ Class Kata extends \Ecc
     h1 {
         page-break-before: always;
     }
+
+    h2 {
+        font-size: 1.75rem !important;
+        page-break-after: avoid;
+    }
     
-    .row {
-        display: flex;
+    h3 {
+        font-size: 1.2rem !important;
+        page-break-after: avoid;
     }
 }
+
+h2 {
+        font-size: 1.75rem !important;
+}
+
+h3 {
+        font-size: 1.2rem !important;
+}
 </style>
+<input name="_noFooter" id="_noFooter" type="hidden" value="1">
 <div class="row">
     <div class="col-12">    
+        <h3>Számlára írni:</h3>
+        <p>Teljesítés: <strong>'.$f3->_teljesitesIdo.'</strong></p>
+        <p>Számla kelte: ma - <strong>'.date('Y.m.d.', time()).'</strong></p>
+        <p>Teljesítés határidő: ma+15nap - <strong>'.date('Y.m.d.', time() + 60*60*24*15).'</strong></p>
+        
         <h1 class="text-center text-uppercase droid">Megbízási szerződés</h1>
+        
+        <h2 class="droid pt-3 pb-2">1. Szerződő felek</h2>
+        
+        <h3 class="droid ml-2 pt-3 pb-2">1.1. Megbízó:</h3>
+        <p class="ml-4">        
+            <strong>'.$f3->_megbizoNev.'</strong>
+        </p>
+        <table class="ml-4 w-100">
+            <tr>
+                <td class="w-25 align-left">Cím:</td>
+                <td>'.$f3->_megbizoCim.'</td>
+            </tr>
+            <tr>
+                <td class="w-25 align-left">Adószám:</td>
+                <td>'.$f3->_megbizoAdoszam.'</td>
+            </tr>
+            <tr>
+                <td class="w-25 align-left">Bankszámlaszám:</td>
+                <td>'.$f3->_megbizoBankszamla.'</td>
+            </tr>
+            <tr>
+                <td class="w-25 align-left">A továbbiakban:</td>
+                <td><strong>Megbízó</strong></td>
+            </tr>
+        </table>
+
+        <h3 class="droid ml-2 pt-3 pb-2">1.2. Megbízott:</h3>
+        <p class="ml-4">        
+            <strong>'.$f3->_megbizottNev.'</strong>, KATA szerinti egyéni vállalkozó
+        </p>
+        <table class="ml-4 w-100">
+            <tr>
+                <td class="w-25 align-left">Cím:</td>
+                <td>'.$f3->_megbizottCim.'</td>
+            </tr>
+            <tr>
+                <td class="w-25 align-left">Adószám:</td>
+                <td>'.$f3->_megbizottAdoszam.'</td>
+            </tr>
+            <tr>
+                <td class="w-25 align-left">Bankszámlaszám:</td>
+                <td>'.$f3->_megbizottBankszamla.'</td>
+            </tr>
+            <tr>
+                <td class="w-25 align-left">A továbbiakban:</td>
+                <td><strong>Megbízott</strong></td>
+            </tr>
+        </table>
+
+        <h2 class="droid pt-3 pb-2">2. Szerződés tárgya</h2>
+        <p class="ml-4">
+            <strong><em>'.$f3->_targy.'</em></strong>
+        </p>
+        
+        <h2 class="droid pt-3 pb-2">3. A megbízás határideje</h2>
+        <p class="ml-4">
+            <strong>'.$f3->_hatarido.'</strong>
+        </p>
+        
+        <h2 class="droid pt-3 pb-2">4. A megbízás díja</h2>
+        <h3 class="droid ml-2">4.1.</h3>
+        <p class="ml-4">
+           A Megbízottat a 2. pontban megjelölt munkáért az alábbi megbízási díj illeti meg:
+        </p>
+        <p class="ml-5">
+            Megbízási díj: <strong>'.$f3->_dijSzam.' Ft</strong> (azaz '.$f3->_dijBetu.' forint), ÁFA alanyi mentes.
+        </p>
+        <h3 class="droid ml-2">4.2.</h3>
+        <p class="ml-4">
+            A megbízási díj tartalmaz valamennyi, Megbízott által felszámítható költséget, így további költségtérítésnek – a Felek eltérő rendelkezései hiányában - nincs helye.
+        </p>
+        <h3 class="droid ml-2">4.3.</h3>      
+        <p class="ml-4">
+            Megbízott számláját a megbízás utolsó napjának megfelelő teljesítési időponttal jogosult benyújtani.
+        </p>
+        <section>        
+            <h2 class="droid pt-3 pb-2">5. A szerződés érvényessége</h2>
+            <h3 class="droid ml-2">5.1.</h3>
+            <p class="ml-4">
+                A szerződés a Felek cégszerű aláírásakor lép érvénybe.
+            </p>
+            <h3 class="droid ml-2">5.2.</h3>
+            <p class="ml-4">
+               A szerződést a Felek csak írásban, közös akarattal módosíthatják.
+            </p>
+        </section>
+        
+        <h2 class="droid pt-3 pb-2">6. Teljesítés</h2>
+        <h3 class="droid ml-2">6.1.</h3>
+        <p class="ml-4">
+            Amennyiben a Megbízónak felróható okból hiúsul meg Megbízott bármely kötelezettségének teljesítése, a Megbízott jogosult a szolgáltatást időarányosan elszámolni, és az időarányos díjat Megbízónak leszámlázni.
+        </p>
+        
+        <section>        
+            <h2 class="droid pt-3 pb-2">7. A szerződés felmondása</h2>
+            <h3 class="droid ml-2">7.1.</h3>
+            <p class="ml-4">
+                Felek jogosultak a szerződés felmondására, ha másik Fél fizetésképtelenné válik, csődbe megy, felszámolják vagy feloszlatják.
+            </p>
+            <h3 class="droid ml-2">7.2.</h3>
+            <p class="ml-4">
+                Felek jogosultak a szerződést azonnali hatállyal felbontani, ha a másik Fél nyilvánvalóan durva, vagy ismételt szerződésszegést követ el, vagy nyilvánvalóan megszegi a jelen szerződésre alkalmazandó jogszabályokat és ezeket nem orvosolja haladéktalanul.
+            </p>
+        </section>
+        
+        <h2 class="droid pt-3 pb-2">8. Védelmi- és információs jog</h2>
+        <h3 class="droid ml-2">8.1.</h3>
+        <p class="ml-4">
+            Megbízó köteles a Megbízottat a tevékenysége sikeres kifejtéséhez szükséges információkhoz juttatni, a szükséges adatokat rendelkezésre bocsátani.
+        </p>
+        <h3 class="droid ml-2">8.2.</h3>
+        <p class="ml-4">
+            Megbízott köteles a közreműködése következtében tudomására jutott adatokat – így különösen az üzleti titok kategóriájában tartozó információkat – bizalmasan kezelni, azokat harmadik személy részére nem szolgáltathatja ki.
+        </p>
+        
+        <h2 class="droid pt-3 pb-2">9. Fizetési feltételek</h2>
+        <h3 class="droid ml-2">9.1.</h3>
+        <p class="ml-4">
+            Megbízó köteles Megbízott számlája alapján a megbízási díjat '.$f3->_dijHatarido.' napon belül Megbízott <strong>'.$f3->_megbizottBankszamla.'</strong> számú bankszámlájára átutalni.
+        </p>
+        
+        <h2 class="droid pt-3 pb-2">10. Egyéb feltételek</h2>
+        <h3 class="droid ml-2">10.1.</h3>
+        <p class="ml-4">
+            Jelen szerződésben nem szabályozott kérdésekben a Polgári Törvénykönyv megbízásra vonatkozó rendelkezései az irányadók.
+        </p>
+        
+        <p class="" style="margin-top: 150px;">
+            '.$f3->_szerzodesHelye.', '.$f3->_szerzodesIdo.'
+        </p>
+        
+        <p class="text-center" style="margin-top: 150px;">
+            <span class="w-25 border-top px-5" style="margin-right: 200px;"><strong><em>Megbízó</em></strong></span>
+            <span class="w-25 border-top px-5"><strong><em>Megbízott</em></strong></span>
+        </p>
+        
+        <h1 class="text-center text-uppercase droid mb-5">Teljesítésigazolás</h1>
+        
+        <table class="w-100 mt-5">
+            <tr>
+                <td class="w-25 align-left">Megbízó:</td>
+                <td><strong>'.$f3->_megbizoNev.'</strong></td>
+            </tr>
+            <tr>
+                <td class="w-25 align-left">Cím:</td>
+                <td>'.$f3->_megbizoCim.'</td>
+            </tr>
+        </table>
+        
+        <table class="w-100 mt-3">
+            <tr>
+                <td class="w-25 align-left">Megbízott:</td>
+                <td><strong>'.$f3->_megbizottNev.'</strong>, KATA szerinti egyéni vállalkozó</td>
+            </tr>
+            <tr>
+                <td class="w-25 align-left">Cím:</td>
+                <td>'.$f3->_megbizottCim.'</td>
+            </tr>
+        </table>
+        
+        <p class="mt-3">
+            Tárgy: <em>'.$f3->_targy.'</em>
+        </p>
+        
+        <p class="mt-3">
+            Megbízott mai napon leszállította, elvégezte tárgyi munkát. Megbízó elismeri a teljesítést.
+            <br>A teljesítés alapján Megbízott <strong>'.$f3->_dijSzam.' Ft</strong>, ÁFA alanyi mentes számla benyújtására jogosult.
+        </p>
+        
+        <p class="" style="margin-top: 150px;">
+            '.$f3->_szerzodesHelye.', '.$f3->_teljesitesIdo.'
+        </p>
+        
+        <p class="text-center" style="margin-top: 150px;">
+            <span class="w-25 border-top px-5"><strong><em>Megbízó</em></strong></span>
+        </p>
+        
     </div>
 </div>
-    
-<div class="row">
-    <div class="col-12">    
-        <h2 class="droid">1. Szerződő felek</h2>
-    </div>
-</div>
-    
-    <div class="row">
-        <div class="col-11 offset-1">
-            <h3 class="droid">1.1. Megbízó:</h3>
-        </div>
-    </div>
-    
-        <div class="row">
-            <div class="col-8 offset-4">    
-                <strong>'.$f3->_megbizoNev.'</strong>
-            </div>
-        </div>
-        
-        <div class="row">
-            <div class="col-2 offset-2 text-right">    
-                Cím:
-            </div>
-            <div class="col-8 d-block">    
-                '.$f3->_megbizoCim.'
-            </div>
-        </div>
-        
-        <div class="row">
-            <div class="col-2 offset-2 text-right">    
-                Adószám:
-            </div>
-            <div class="col-8">    
-                '.$f3->_megbizoAdoszam.'
-            </div>
-        </div>
-        
-        <div class="row">
-            <div class="col-2 offset-2 text-right">    
-                Bankszámlaszám:
-            </div>
-            <div class="col-8">    
-                '.$f3->_megbizoBankszamla.'
-            </div>
-        </div>
-        
-        <div class="row">
-            <div class="col-10 offset-2 text-right">    
-                A továbbiakban: <strong>Megbízó</strong>
-            </div>
-        </div>
-        
-    <div class="row">
-        <div class="col-11 offset-1">
-            <h3 class="droid">1.2. Megbízott:</h3>
-        </div>
-    </div>
-    
-        <div class="row">
-            <div class="col-8 offset-4">    
-                <strong>'.$f3->_megbizottNev.'</strong>, KATA szerinti egyéni vállalkozó
-            </div>
-        </div>
-        
-        <div class="row">
-            <div class="col-2 offset-2 text-right">    
-                Cím:
-            </div>
-            <div class="col-8">    
-                '.$f3->_megbizottCim.'
-             </div>
-        </div>
-        
-        <div class="row">
-            <div class="col-2 offset-2 text-right">    
-                Adószám:
-            </div>
-            <div class="col-8">    
-                '.$f3->_megbizottAdoszam.'
-            </div>
-        </div>
-        
-        <div class="row">
-            <div class="col-2 offset-2 text-right">    
-                Bankszámlaszám:
-            </div>
-            <div class="col-8">    
-                '.$f3->_megbizottBankszamla.'
-            </div>
-        </div>
-        
-        <div class="row">
-            <div class="col-10 offset-2 text-right">    
-                A továbbiakban: <strong>Megbízott</strong>
-            </div>
-        </div>
-    
-<div class="row">
-    <div class="col-12">    
-        <h2 class="droid">2. Szerződés tárgya</h2>
-    </div>
-</div>    
-
-    <div class="row">
-        <div class="col-11 offset-1">
-            <p><strong><em>'.$f3->_targy.'</em></strong></p>
-        </div>
-    </div>
-
-<div class="row">
-    <div class="col-12">    
-        <h2 class="droid">3. A megbízás határideje</h2>
-    </div>
-</div>    
-
-    <div class="row">
-        <div class="col-11 offset-1">
-            <p><strong><em>'.$f3->_hatarido.'</em></strong></p>
-        </div>
-    </div>
-    
-<div class="row">
-    <div class="col-12">    
-        <h2 class="droid">4. A megbízás díja</h2>
-    </div>
-</div>    
-
-    <div class="row">
-        <div class="col-11 offset-1">
-            <p>
-                <strong>4.1.</strong> A Megbízottat a 2. pontban megjelölt munkáért az alábbi megbízási díj illeti meg:
-            </p>
-            <p class="ml-5">
-                Megbízási díj: <strong>'.$f3->_dijSzam.' Ft</strong> (azaz '.$f3->_dijBetu.' forint), ÁFA alanyi mentes.
-            </p>
-            <p>
-                <strong>4.2.</strong> A megbízási díj tartalmaz valamennyi, Megbízott által felszámítható költséget, így további költségtérítésnek – a Felek eltérő rendelkezései hiányában - nincs helye.
-            </p>      
-            <p>
-                <strong>4.3.</strong> 4.3.	Megbízott számláját a megbízás utolsó napjának megfelelő teljesítési időponttal jogosult benyújtani.
-            </p>     
-        </div>
-    </div> 
-
-<div class="row">
-    <div class="col-12">    
-        <h2 class="droid">5. A szerződés érvényessége</h2>
-    </div>
-</div>   
-    <div class="row">
-        <div class="col-11 offset-1">
-            <p>
-                <strong>5.1.</strong> A szerződés a Felek cégszerű aláírásakor lép érvénybe.
-            </p>
-            <p>
-                <strong>5.2.</strong> A szerződést a Felek csak írásban, közös akarattal módosíthatják.
-            </p>
-        </div>
-    </div> 
-    
-<div class="row">
-    <div class="col-12">    
-        <h2 class="droid">6. Teljesítés</h2>
-    </div>
-</div>   
-    <div class="row">
-        <div class="col-11 offset-1">
-            <p>
-                <strong>6.1.</strong>Amennyiben a Megbízónak felróható okból hiúsul meg Megbízott bármely kötelezettségének teljesítése, a Megbízott jogosult a szolgáltatást időarányosan elszámolni, és az időarányos díjat Megbízónak leszámlázni.
-            </p>
-        </div>
-    </div> 
-    
-<div class="row">
-    <div class="col-12">    
-        <h2 class="droid">7. A szerződés felmondása</h2>
-    </div>
-</div>   
-    <div class="row">
-        <div class="col-11 offset-1">
-            <p>
-                <strong>7.1.</strong> Felek jogosultak a szerződés felmondására, ha másik Fél fizetésképtelenné válik, csődbe megy, felszámolják vagy feloszlatják.
-            </p>
-            <p>
-                <strong>7.2.</strong> Felek jogosultak a szerződést azonnali hatállyal felbontani, ha a másik Fél nyilvánvalóan durva, vagy ismételt szerződésszegést követ el, vagy nyilvánvalóan megszegi a jelen szerződésre alkalmazandó jogszabályokat és ezeket nem orvosolja haladéktalanul.
-            </p>
-        </div>
-    </div> 
-    
-<div class="row">
-    <div class="col-12">    
-        <h2 class="droid">8. Védelmi- és információs jog</h2>
-    </div>
-</div>   
-    <div class="row">
-        <div class="col-11 offset-1">
-            <p>
-                <strong>8.1.</strong> Megbízó köteles a Megbízottat a tevékenysége sikeres kifejtéséhez szükséges információkhoz juttatni, a szükséges adatokat rendelkezésre bocsátani.
-            </p>
-            <p>
-                <strong>8.2.</strong> Megbízott köteles a közreműködése következtében tudomására jutott adatokat – így különösen az üzleti titok kategóriájában tartozó információkat – bizalmasan kezelni, azokat harmadik személy részére nem szolgáltathatja ki.
-            </p>
-        </div>
-    </div> 
-    
-<div class="row">
-    <div class="col-12">    
-        <h2 class="droid">9. Fizetési feltételek</h2>
-    </div>
-</div>   
-    <div class="row">
-        <div class="col-11 offset-1">
-            <p>
-                <strong>9.1.</strong> Megbízó köteles Megbízott számlája alapján a megbízási díjat '.$f3->_dijHatarido.' napon belül Megbízott <strong>'.$f3->_megbizottBankszamla.'</strong> számú bankszámlájára átutalni.
-            </p>
-        </div>
-    </div> 
-
-<div class="row">
-    <div class="col-12">    
-        <h2 class="droid">10. Egyéb feltételek</h2>
-    </div>
-</div>   
-    <div class="row">
-        <div class="col-11 offset-1">
-            <p>
-                <strong>10.1.</strong> Jelen szerződésben nem szabályozott kérdésekben a Polgári Törvénykönyv megbízásra vonatkozó rendelkezései az irányadók.
-            </p>
-        </div>
-    </div>
-    
-<div class="row mt-5">
-    <div class="col-12">    
-        '.$f3->_szerzodesHelye.', '.$f3->_szerzodesIdo.'
-    </div>
-</div>   
-
-<div class="row mt-5">
-    <div class="col-3 offset-2 mp-5">    
-        <p class="text-center border-top"><strong><em>Megbízó</em></strong></p>
-    </div>
-    <div class="col-3 offset-2 mp-5">    
-        <p class="text-center border-top"><strong><em>Megbízott</em></strong></p>
-    </div>
-</div>  
         ';
         $blc->html($html);
     }
