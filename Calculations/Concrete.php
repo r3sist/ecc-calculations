6a0bfa58664019dece81f71276fb4701104ecfa1
resist <vankos@resist.hu>
2018-10-16 11:15:58 +0200

    Concrete: material data support; smaller improvements

--- Calculations/Concrete.php
+++ Calculations/Concrete.php
@@ -9,23 +9,13 @@ Class Concrete extends \Ecc
     {
         $ec = \Ec::instance();
         $blc = \Blc::instance();
-        $lava = new \Khill\Lavacharts\Lavacharts;
 
-        $blc->note('A számítások Tóth Bertalan programja alapján történnek: https://structure.hu/berci/material ');
+        $blc->note('A számítások [Tóth Bertalan programja](https://structure.hu/berci/material) alapján történnek.');
 
         $ec->matList('mat', 'C25/30', 'Beton anyag');
-
+        $ec->saveMaterialData($f3->_mat, false);
+        $blc->txt('', 'A fent megadott anyagjellemzők a beton 28 napos korában érvényesek.');
         $blc->note('A szilárdsági osztályhoz tartozó jellemzők a 28 napos korban meghatározott, hengeren mért nyomószilárdság fck karakterisztikus értékén alapulnak.');
-
-        $blc->h1('Beton jellemzői 28 napos korban');
-
-        $blc->def('fck', $ec->matProp($f3->_mat, 'fck'), 'f_(c,k(0.05)) = %% [MPa]', 'Nyomószilárdság karakterisztikus értéke (5% kvantilis)');
-        $blc->def('fcm', $ec->matProp($f3->_mat, 'fcm'), 'f_(c,m) = %% [MPa]', 'Nyomószilárdság várható értéke');
-        $blc->def('fctm', $ec->matProp($f3->_mat, 'fctm'), 'f_(c,t,m) = %% [MPa]', 'Húzószilárdság várható értéke - táblázatos');
-        $blc->def('fctm_calc', number_format(0.3*pow($f3->_fck,2/3), 2), 'f_(c,t,m) = 0.3*f_(c,k)^(2/3) = %% [MPa]', 'Húzószilárdság várható értéke - számított');
-        $blc->def('fctk005', $ec->matProp($f3->_mat, 'fctk005'), 'f_(c,t,k(0.05)) = %% [MPa]', 'Húzószilárdság karakterisztikus értéke (5% kvantilis)');
-        $blc->def('fctk095', $ec->matProp($f3->_mat, 'fctk095'), 'f_(c,t,k(0.95)) = %% [MPa]', 'Húzószilárdság karakterisztikus értéke (95% kvantilis)');
-        $blc->def('Ecm', $ec->matProp($f3->_mat, 'Ecm'), 'E_(c,m) = %% [GPa]', 'Rugalmassági modulus');
 
         $blc->h1('Beton jellemzői `t` napos korban');
         $blc->input('t', 'Idő', '10', 'nap', '', false);
@@ -40,25 +30,25 @@ Class Concrete extends \Ecc
         $blc->note('R: nagy kezdő szilárdság; N: normál kezdő szilárdság');
         $blc->lst('cem', $cem, 'Cement típus', 0.25, '', false);
         $blc->def('betacc', exp($f3->_cem*(1-sqrt(28/$f3->_t))), 'beta_c = %%');
-        $blc->def('fcmt', number_format($f3->_fcm*$f3->_betacc, 2), 'f_(c,m)(t) = beta_c*f_(c,m) = %% [MPa]', 'Nyomószilárdság várható értéke');
+        $blc->def('fcmt', number_format($f3->_fcm*$f3->_betacc, 2), 'f_(c,m)(t) = beta_c*f_(c,m) = %% [N/(mm^2)]', 'Nyomószilárdság várható értéke');
         $y = $f3->_fck;
         if ($f3->_t < 28) {
             $y = ($f3->_fcmt - 8);
         }
-        $blc->def('fckt', number_format($y, 2), 'f_(c,k)(t) = %% [MPa]', 'Nyomószilárdság karakterisztikus értéke (5% kvantilis)');
+        $blc->def('fckt', number_format($y, 2), 'f_(c,k)(t) = %% [N/(mm^2)]', 'Nyomószilárdság karakterisztikus értéke (5% kvantilis)');
         $y2 = 2/3;
         if ($f3->_t < 28) {
             $y2 = 1;
         }
-        $blc->def('fctmt', number_format(pow($f3->_betacc, $y2)*$f3->_fctm_calc, 2), 'f_(c,t,m)(t) = %% [MPa]', 'Húzószilárdság várható értéke');
-        $blc->def('fctk005t', number_format(0.7*$f3->_fctmt, 2), 'f_(c,t,k(0.05))(t) = 0.7*f_(c,t,m)(t) = %% [MPa]', 'Húzószilárdság karakterisztikus értéke (5% kvantilis)');
-        $blc->def('fctk095t', number_format(1.3*$f3->_fctmt, 2), 'f_(c,t,k(0.95))(t) = 1.3*f_(c,t,m)(t) = %% [MPa]', 'Húzószilárdság karakterisztikus értéke (95% kvantilis)');
-        $blc->def('Ecmt', number_format(pow($f3->_fcmt/$f3->_fcm, 0.3)*$f3->_Ecm, 2), 'E_(c,m)(t) = %% [GPa]', 'Rugalmassági modulus');
+        $blc->def('fctmt', number_format(pow($f3->_betacc, $y2)*$f3->_fctm, 2), 'f_(c,t,m)(t) = %% [N/(mm^2)]', 'Húzószilárdság várható értéke');
+        $blc->def('fctk005t', number_format(0.7*$f3->_fctmt, 2), 'f_(c,t,k(0.05))(t) = 0.7*f_(c,t,m)(t) = %% [N/(mm^2)]', 'Húzószilárdság karakterisztikus értéke (5% kvantilis)');
+        $blc->def('fctk095t', number_format(1.3*$f3->_fctmt, 2), 'f_(c,t,k(0.95))(t) = 1.3*f_(c,t,m)(t) = %% [N/(mm^2)]', 'Húzószilárdság karakterisztikus értéke (95% kvantilis)');
+        $blc->def('Ecmt', number_format(pow($f3->_fcmt/$f3->_fcm, 0.3)*$f3->_Ecm, 2), 'E_(c,m)(t) = %% [(kN)/(mm^2)]', 'Rugalmassági modulus');
 
         $blc->jsxDriver();
         $js = '
 
-var board = JXG.JSXGraph.initBoard("test", {boundingbox: [-3,70,40,-8], axis:true, showCopyright:false, keepaspectratio: false, showNavigation: true});
+var board = JXG.JSXGraph.initBoard("concrete", {boundingbox: [-3,70,40,-8], axis:true, showCopyright:false, keepaspectratio: false, showNavigation: true});
 
 							var x = '.$f3->_fck.';
 							var s = '.$f3->_cem.';
@@ -86,6 +76,6 @@ var board = JXG.JSXGraph.initBoard("test", {boundingbox: [-3,70,40,-8], axis:tru
 							//board.create(\'line\', [q,p6],{strokeColor:"red", strokeWidth:2, dash:1});
 						
 ';
-        $blc->jsx('test', $js);
+        $blc->jsx('concrete', $js);
     }
 }
