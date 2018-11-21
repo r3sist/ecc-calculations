557690b6b03556892ebfb324873e1ced55b1ace4
resist <vankos@resist.hu>
2018-11-21 16:56:47 +0100

    Weld: add return type hint to calc method

--- Calculations/Weld.php
+++ Calculations/Weld.php
@@ -11,7 +11,7 @@ Class Weld extends \Ecc
      * @var $ec \Ec\Ec
      * @throws \Exception
      */
-    public function calc($f3, $blc, $ec)
+    public function calc(object $f3, object $blc, object $ec): void
     {
         $blc->input('a', 'Varrat gyökméret', '4', 'mm', '', 'numeric');
         $blc->input('L', 'Varrat összes hossz', '100', 'mm', '', 'numeric');
