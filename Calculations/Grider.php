2a1f0745a00bbd3d078f31b5d76393b2f3650305
resist <vankos@resist.hu>
2018-11-26 17:15:29 +0100

    Grider: init

---
+++ Calculations/Grider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Calculation;
+
+Class Grider extends \Ecc
+{
+
+    /**
+     * @var $f3 \Base
+     * @var $blc \Blc
+     * @var $ec \Ec\Ec
+     * @throws \Exception
+     */
+    public function calc(object $f3, object $blc, object $ec): void
+    {
+
+    }
+}
