7ab1ed525e3ba0e07d98771b47059868fdfad1e5
resist <vankos@resist.hu>
2018-12-13 09:49:47 +0100

    Config: dev GUI

--- Calculations/Config.php
+++ Calculations/Config.php
@@ -13,13 +13,13 @@ Class Config extends \Ecc
      */
     public function calc(object $f3, object $blc, object $ec): void
     {
-        $blc->h1('Beállítások');
-        $blc->boo('nativeMath', 'Szerveroldali ASCIIMath konvertálás MathML formátumba.', false, '');
+        $f3->set('mu', new \DB\SQL\Mapper($f3->get('db'), 'users'));
 
-        $f3->mu->load(array('uid = :uid', ':uid'=>$f3->get('uid')));
-        if (!$this->f3->mu->dry()) {
-            $this->f3->mu->ueccnativemathml = \V3::boo($f3->_nativeMath);
-            $this->f3->mu->save();
+        $blc->boo('nativeMath', 'Szerveroldali ASCIIMath konvertálás MathML formátumba', $f3->udata['ueccnativemathml'], 'A módosítások aktiválásához teljes oldal újratöltése szükséges.');
+        $f3->mu->load(array('uid = :uid', ':uid' => $f3->get('uid')));
+        if (!$f3->mu->dry()) {
+            $f3->mu->ueccnativemathml = \V3::boo($f3->_nativeMath);
+            $f3->mu->save();
         }
     }
 }
