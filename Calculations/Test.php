47ad28ac1318f0121f64307689921dcd3624f7f4
resist <vankos@resist.hu>
2019-01-15 12:52:08 +0100

    Test: add test documentum

---
+++ Calculations/Test.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Calculation;
+
+Class Test extends \Ecc
+{
+
+    /**
+     * @var $f3 \Base
+     * @var $blc \Blc
+     * @var $ec \Ec\Ec
+     * @throws \Exception
+     */
+    public function calc(object $f3, object $blc, object $ec): void
+    {
+        $blc->h1('Teszt dokumentum');
+
+    }
+}
