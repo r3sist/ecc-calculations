859ba69f2bed46b21e99bf9701a93b9c85cd2258
resist <vankos@resist.hu>
2019-01-17 12:12:23 +0100

    Anchor: fix array titles

--- Calculations/Anchor.php
+++ Calculations/Anchor.php
@@ -13,32 +13,32 @@ Class Anchor extends \Ecc
      */
     public function calc(object $f3, object $blc, object $ec): void
     {
-        $ec->matList('aMat','B500', 'Tüske anyagminőség');
-        $ec->matList('cMat','C40/50', 'Beton anyagminőség');
-        $blc->input('D', '`D:` Csapátmérő', '20', 'mm', '');
-        $blc->input('e', '`e:` Beton felületek közti hézag', '10', 'mm', '');
-        $blc->input('c1t', '`c_(1t):` Tüske tengely erő irányra merőlegesen', '100', 'mm', '');
-        $blc->input('c2t', '`c_(2t):` Tüske tengely erő irányban', '100', 'mm', '');
-        $blc->input('n', '`n:` Csapok száma', '2', '', '');
+        $ec->matList('aMat','B500', ['', 'Tüske anyagminőség']);
+        $ec->matList('cMat','C40/50', ['', 'Beton anyagminőség']);
+        $blc->numeric('D', ['D', 'Csapátmérő'], 20, 'mm', '');
+        $blc->numeric('e', ['e', 'Beton felületek közti hézag'], 10, 'mm', '');
+        $blc->numeric('c1t', ['c_(1t)', 'Tüske tengely erő irányra merőlegesen'], 100, 'mm', '');
+        $blc->numeric('c2t', ['c_(2t)', 'Tüske tengely erő irányban'], 100, 'mm', '');
+        $blc->numeric('n', ['n', 'Csapok száma'], 2, '', '');
         $steelPlateDOF = [
             '0' => 0,
             '1' => 1,
             '2' => 2
         ];
-        $blc->lst('n_s', $steelPlateDOF, '`n_s:` Acéllemez befogási tényező', '0');
-        $blc->input('GRd', '`gamma_(Rd):`', '1.3', '', '');
-        $blc->input('FVEd', '`F_(V,Ed):` Mértékadó nyíróerő csap képen', '30', 'kN', '');
+        $blc->lst('n_s', $steelPlateDOF, ['n_s', 'Acéllemez befogási tényező'], '0');
+        $blc->numeric('GRd', ['gamma_(Rd)', ''], 1.3, '', '');
+        $blc->numeric('FVEd', ['F_(V,Ed)', 'Mértékadó nyíróerő csap képen'], 30, 'kN', '');
         $states = [
             'Tartós, ideiglenes' => 1,
             'Földrengés' => 2
         ];
-        $blc->lst('limit', $states, '`li\mit:` Tervezési állapot', 2);
+        $blc->lst('limit', $states, ['', 'Tervezési állapot'], 2);
 
         $blc->region0('r0', 'Számítások');
-            $blc->def('fyk', $ec->matProp($f3->_aMat,'fy'), 'f_(y,k) = %% [MPa]', 'Csap karakterisztikus folyáshatára');
-            $blc->def('fyd', $ec->matProp($f3->_aMat,'fyd'), 'f_(y,d) = %% [MPa]', 'Csap tervezési folyáshatára');
-            $blc->def('fck', $ec->matProp($f3->_cMat,'fck'), 'f_(c,k) = %% [MPa]', 'Beton szilárdság karakterisztikus értéke');
-            $blc->def('fcd', $f3->_fck/$f3->__Gc, 'f_(c,d) = f_(c,k)/gamma_c = %% [MPa]', 'Beton szilárdság tervezési értéke');
+            $blc->def('fyk', $ec->matProp($f3->_aMat,'fy'), 'f_(y,k) = %% [N/(mm^2)]', 'Csap karakterisztikus folyáshatára');
+            $blc->def('fyd', $ec->matProp($f3->_aMat,'fyd'), 'f_(y,d) = %% [N/(mm^2)]', 'Csap tervezési folyáshatára');
+            $blc->def('fck', $ec->matProp($f3->_cMat,'fck'), 'f_(c,k) = %% [N/(mm^2)]', 'Beton szilárdság karakterisztikus értéke');
+            $blc->def('fcd', \H3::n2($f3->_fck/$f3->__Gc), 'f_(c,d) = f_(c,k)/gamma_c = %% [N/(mm^2)]', 'Beton szilárdság tervezési értéke');
             $blc->def('c1', $f3->_c1t - ($f3->_D/2), 'c_1 = c_(1t) - D/2 = %% [mm]', '');
             $blc->def('c2', $f3->_c2t - ($f3->_D/2), 'c_2 = c_(2t) - D/2 = %% [mm]', '');
             $blc->def('c1D', $f3->_c1/$f3->_D, 'c_(1D) = c_1/D = %%', '');
@@ -82,8 +82,8 @@ Class Anchor extends \Ecc
             $blc->def('epsilon', (((3*$f3->_e)/$f3->_epsilon_0)/$f3->_D)*sqrt($f3->_fck/$f3->_fyk), 'epsilon = ((3*e)/epsilon_0)/D*sqrt(f_(ck)/f_(yk)) = %%', '');
             $blc->def('alpha_er', sqrt(pow($f3->_alpha_r, 2) + pow($f3->_epsilon*$f3->_alpha_0, 2)) - $f3->_epsilon*$f3->_alpha_0, 'alpha_(er) = sqrt(alpha_r^2 + (epsilon*alpha_0)^2) - epsilon*alpha_0 = %%', '');
 
-            $blc->math('li\mit = '.$f3->_limit);
-            $blc->def('alpha_s', (-0.35)*$f3->_limit + 1.35, 'alpha_s = -0.35*li\mit + 1.35 = %%');
+            $blc->math('l imit = '.$f3->_limit);
+            $blc->def('alpha_s', (-0.35)*$f3->_limit + 1.35, 'alpha_s = -0.35*l imit + 1.35 = %%');
 
             if ($f3->_n_s == 2) {
                 $blc->def('f', $f3->_fyd*(pow($f3->_D, 3)/(6*$f3->_e)), 'f = (f_(yd)*D^3)/(6*e)) = %% [N]');
@@ -92,9 +92,9 @@ Class Anchor extends \Ecc
             }
         $blc->region1('r0');
 
-        $blc->def('FVRd', $f3->_alpha_s*$f3->_f/1000, 'F_(V,Rd) = alpha_s*f = %% [kN]', 'Nyírási csap ellenállása');
+        $blc->def('FVRd', $f3->_alpha_s*$f3->_f/1000, 'F_(V,Rd) = alpha_s*f = %% [kN]', 'Egy nyírási csap ellenállása');
         $blc->success0('s0');
-            $blc->def('FVRdS', $f3->_FVRd*$f3->_n, 'F_(V,Rd,sum) = F_(V,Rd)*n = %% [kN]', 'Nyírási csapok ellenállása');
+            $blc->def('FVRdS', $f3->_FVRd*$f3->_n, 'F_(V,Rd,sum) = F_(V,Rd)*n = %% [kN]', 'Nyírási *csap kép* ellenállása');
             $blc->label($f3->_FVEd/$f3->_FVRdS, 'Csap kép kihasználtság');
         $blc->success1('s0');
     }
