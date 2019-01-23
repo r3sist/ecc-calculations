8d7870dbba7053f31c1f13ff6241ba446c6a3f2f
resist <vankos@resist.hu>
2019-01-23 09:50:22 +0100

    JAVÍTÁS: címek; math

--- Calculations/LinQ.php
+++ Calculations/LinQ.php
@@ -14,10 +14,10 @@ Class LinQ extends \Ecc
     public function calc(object $f3, object $blc, object $ec): void
     {
         $blc->note('Számítás apropója, hogy gerenda irányra merőlegesen, egyenletesen változó megoszló felületi teher (pl. hózug teher) szétosztást a Consteel nem tud kezelni. A felületi teher vonalmenti teherré alakítható a lenti számítás alapján.');
-        $blc->input('x', '`x"[ ]":` Gerenda koordináták', '2,6,10,14', 'm', 'Gerenda `x_i` pozíciói (koordinátái), vesszővel elválasztva, 0-tól számítva.');
-        $blc->input('xMAX', '`x_(max):` Teher felület hossza', '16', 'm', '');
-        $blc->input('qMAX', '`q_(max):` Megoszló teher (növekvő) maximális értéke', '10', 'kN/m²', '');
-        $blc->input('qMIN', '`x_(min):` Megoszló teher minimális értéke', '2', 'kN/m²', '');
+        $blc->input('x', ['x_i,x_j,..', 'Gerenda koordináták'], '2,6,10,14', 'm', 'Gerenda $x$ pozíciói (koordinátái), vesszővel elválasztva, $0[m]$-től számítva.');
+        $blc->input('xMAX', ['x_(max)', 'Teher felület hossza'], '16', 'm', '');
+        $blc->input('qMAX', ['q_(max)', 'Megoszló teher (növekvő) maximális értéke'], '10', 'kN/m²', '');
+        $blc->input('qMIN', ['x_(min)', 'Megoszló teher minimális értéke'], '2', 'kN/m²', '');
 
         $f3->set('_qd',$f3->_qMAX - $f3->_qMIN);
         $f3->set('_q1',$f3->_qd / $f3->_xMAX);
@@ -28,19 +28,21 @@ Class LinQ extends \Ecc
         }
         $table = array();
         $i = 0;
+        $colNameA = 'Mező $b [m]$';
+        $colNameB = 'Vonalmenti teher $p [(kN)/m]$';
         foreach ($x as $xAct) {
             if (isset($x[$i + 1]) && $x[$i + 1] <= $x[$i]) {
                 $blc->danger('Hibás koordináta sorrend vagy fedő gerendák.', 'Hiba!');
             }
             if ($i == 0) {
-                $table[$xAct]['b [m]'] = \H3::n2(($x[$i + 1] - $x[$i])/2 + $x[$i]);
-                $table[$xAct]['p [kN/m]'] = \H3::n2((((($x[$i + 1] - $x[$i])/2 + $x[$i])*$f3->_q1)/2 + $f3->_qMIN)*$table[$xAct]['b [m]']);
+                $table[$xAct][$colNameA] = \H3::n2(($x[$i + 1] - $x[$i])/2 + $x[$i]);
+                $table[$xAct][$colNameB] = \H3::n2((((($x[$i + 1] - $x[$i])/2 + $x[$i])*$f3->_q1)/2 + $f3->_qMIN)*$table[$xAct][$colNameA]);
             } elseif ($i == count($x) - 1) {
-                $table[$xAct]['b [m]'] = \H3::n2(($x[$i] - $x[$i - 1])/2 + $f3->_xMAX - $x[$i]);
-                $table[$xAct]['p [kN/m]'] = \H3::n2((((($x[$i] - ($x[$i] - $x[$i - 1])/2 + $f3->_xMAX))*$f3->_q1)/2 + $f3->_qMIN)*$table[$xAct]['b [m]']);
+                $table[$xAct][$colNameA] = \H3::n2(($x[$i] - $x[$i - 1])/2 + $f3->_xMAX - $x[$i]);
+                $table[$xAct][$colNameB] = \H3::n2((((($x[$i] - ($x[$i] - $x[$i - 1])/2 + $f3->_xMAX))*$f3->_q1)/2 + $f3->_qMIN)*$table[$xAct][$colNameA]);
             } else {
-                $table[$xAct]['b [m]'] = \H3::n2(($x[$i + 1] - $x[$i])/2 + ($x[$i] - $x[$i - 1])/2);
-                $table[$xAct]['p [kN/m]'] = \H3::n2((((( $x[$i] - (($x[$i] - $x[$i - 1])/2) + $x[$i] + (($x[$i + 1] - $x[$i])/2) ))*$f3->_q1)/2 + $f3->_qMIN)*$table[$xAct]['b [m]']);
+                $table[$xAct][$colNameA] = \H3::n2(($x[$i + 1] - $x[$i])/2 + ($x[$i] - $x[$i - 1])/2);
+                $table[$xAct][$colNameB] = \H3::n2((((( $x[$i] - (($x[$i] - $x[$i - 1])/2) + $x[$i] + (($x[$i + 1] - $x[$i])/2) ))*$f3->_q1)/2 + $f3->_qMIN)*$table[$xAct][$colNameA]);
             }
             $i++;
         }
