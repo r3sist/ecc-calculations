35d2083bce5a91f68e1fb6ef1ac65c2b2b81abea
resist <vankos@resist.hu>
2019-01-23 10:06:53 +0100

    MÓDOSÍTVA: címsorok ÚJ: JAVÍTVA: címek; math

--- Calculations/SteelSection.php
+++ Calculations/SteelSection.php
@@ -14,26 +14,34 @@ Class SteelSection extends \Ecc
     public function calc(object $f3, object $blc, object $ec): void
     {
         $blc->toc();
+
+        $blc->info0('F');
+        $blc->numeric('VEd', ['V_(Ed)', 'Nyíróerő'], 100, 'kN');
+        $blc->numeric('MEd', ['M_(Ed)', 'Hajlítónyomaték'], 100, 'kNm');
+        $blc->numeric('NEd', ['N_(Ed)', 'Húzóerő'], 200, 'kN');
+        $blc->info1('F');
+
         $ec->sectionFamilyList();
         $ec->sectionList($f3->_sectionFamily);
         $ec->saveSectionData($f3->_sectionName, true);
 
         $ec->matList('mat', 'S235', 'Acél anyagminőség');
         $ec->saveMaterialData($f3->_mat, false);
-        $blc->input('t', 'Lemezvastagság', 10, 'mm');
-        $blc->txt($f3->_sectionName.' legnagyobb lemezvastagsága: `t_(max) = '. 10*max($f3->_sectionData['tf'], $f3->_sectionData['tw']).' [mm]`');
+        $blc->numeric('t', ['t', 'Lemezvastagság'], 10, 'mm');
+        $blc->txt('*'.$f3->_sectionName.'* legnagyobb lemezvastagsága: $t_(max) = '. 10*max($f3->_sectionData['tf'], $f3->_sectionData['tw']).' [mm]$');
 
         if ($f3->_t >= 40) {
             $f3->_fy = $f3->_fy40;
             $f3->_fu = $f3->_fu40;
-            $blc->txt('`f_y` és `f_u` lemezvastagság miatt csökkentve.');
+            $blc->txt('$f_y$ és $f_u$ lemezvastagság miatt csökkentve.');
         }
 
         $blc->region0('rEgyedi', 'Egyedi keresztmetszeti paraméterek megadása');
-            $blc->input('A_v', 'Egyedi nyírási keresztmetszet', 0, 'mm²', 'Az itt megadott nemnulla érték felülírja a betöltött szelvény adatokat!');
-            $blc->input('A', 'Egyedi húzási keresztmetszet', 0, 'mm²', 'Az itt megadott nemnulla érték felülírja a betöltött szelvény adatokat!');
-            $blc->input('Wpl', 'Egyedi képlékeny keresztmetszeti modulus', 0, 'mm³', 'Az itt megadott nemnulla érték felülírja a betöltött szelvény adatokat!');
-            $blc->input('Wel', 'Egyedi rugalmas keresztmetszeti modulus', 0, 'mm³', 'Az itt megadott nemnulla érték felülírja a betöltött szelvény adatokat!');
+            $blc->txt('Az itt megadott *nemnulla* érték felülírja a betöltött szelvény adatokat.');
+            $blc->numeric('A_v', ['A_v', 'Egyedi nyírási keresztmetszet'], 0, 'mm²', '');
+            $blc->numeric('A', ['A', 'Egyedi húzási keresztmetszet'], 0, 'mm²', '');
+            $blc->numeric('Wpl', ['W_(pl)', 'Egyedi képlékeny keresztmetszeti modulus'], 0, 'mm³', '');
+            $blc->numeric('Wel', ['W_(el)', 'Egyedi rugalmas keresztmetszeti modulus'], 0, 'mm³', '');
         $blc->region1('rEgyedi');
 
         if ($f3->_A_v == 0) {
@@ -53,50 +61,54 @@ Class SteelSection extends \Ecc
         }
 
         $blc->region0('r0', 'Nettó keresztmetszet számítás, csavarszám megadása');
-            $blc->input('n', 'Csavarok száma', 0, '', 'Nemnulla esetén \`A_(n\et)\` számítása innen');
+            $blc->numeric('n', 'Csavarok száma', 0, '', 'Nemnulla esetén $A_(n et)$ számítása innen');
             $ec->boltList('btName');
             $d0 = $ec->boltProp($f3->_btName, 'd0');
-            $blc->def('A_net_calculated', $f3->_A - $f3->_n*$d0*$f3->_t, 'A_(n\et, calcu\lated) = A - n*d_0*t = %% [mm^2]', 'Számított nettó keresztmetszet');
+            if ($f3->_n != 0) {
+                $blc->def('A_net_calculated', $f3->_A - $f3->_n*$d0*$f3->_t, 'A_(n et, calcu lated) = A - n*d_0*t = %% [mm^2]', 'Számított nettó keresztmetszet');
+            }
         $blc->region1('r0');
         if ($f3->_n >0 ) {
-            $blc->def('A_net', $f3->_A_net_calculated, 'A_(n\et) = %% [mm^2]');
+            $blc->def('A_net', $f3->_A_net_calculated, 'A_(n et) = %% [mm^2]');
         } else {
-            $blc->input('A_net', '`A_(n\et):` Lyukkal gyengített keresztmetszet', 0.8*$f3->_A, '`mm^2`');
+            $blc->numeric('A_net', ['A_(n et)', 'Lyukkal gyengített keresztmetszet'], 0.8*$f3->_A, 'mm2');
         }
 
         $blc->h1('Nyírt keresztmetszet');
         $blc->note('[Szürke 2007: 5.1.5 41.o]');
         $blc->success0('v');
-        $blc->def('VplRd', $ec->VplRd($f3->_A_v, $f3->_mat, $f3->_t), 'V_(c,Rd) = V_(pl,Rd) = (A_v*f_y)/(sqrt(3)*gamma_(M0)) = %% [kN]', 'Nyírási ellenállás 1 és 2. kmo. esetén');
+            $blc->def('VplRd', \H3::n2($ec->VplRd($f3->_A_v, $f3->_mat, $f3->_t)), 'V_(c,Rd) = V_(pl,Rd) = (A_v*f_y)/(sqrt(3)*gamma_(M0)) = %% [kN]', 'Nyírási ellenállás 1 és 2. kmo. esetén');
         $blc->success1('v');
-        $blc->note('3 és 4. kmo. esetén `V_(c,Rd) = (A_w*f_y)/(sqrt(3)*gamma_(M0))` H és I szelvénynél. Általános esetben `(I*t)/S*f_y/(sqrt(3)*gamma_(M0))`');
-
-        $blc->input('VEd', '`V_(Ed)` nyíróerő', 100, 'kN');
+        $blc->note('3 és 4. kmo. esetén $V_(c,Rd) = (A_w*f_y)/(sqrt(3)*gamma_(M0))$ *H* és *I* szelvénynél. Általános esetben $(I*t)/S*f_y/(sqrt(3)*gamma_(M0))$');
         $blc->label($f3->_VEd/$f3->_VplRd, 'Nyírási kihasználtság');
+        $blc->txt('', '$V_(Ed)/V_(pl,Rd)$');
 
         $blc->h1('Hajlított keresztmetszet');
         $blc->note('[Szürke 2007: 5.1.4 41.o]');
         $blc->success0('McRdpl');
-        $blc->def('McRdpl', $ec->McRd($f3->_Wpl, $f3->_fy), 'M_(c,Rd) = (W_(pl)*f_y)/gamma_(M0) = %% [kNm]', '1 és 2. kmo. esetén');
+        $blc->def('McRdpl', \H3::n2($ec->McRd($f3->_Wpl, $f3->_fy)), 'M_(c,Rd,pl) = (W_(pl)*f_y)/gamma_(M0) = %% [kNm]', '1 és 2. kmo. esetén');
         $blc->success1('McRdpl');
-        $blc->def('McRdel', $ec->McRd($f3->_Wel, $f3->_fy), 'M_(c,Rd) = (W_(el)*f_y)/gamma_(M0) = %% [kNm]', '3. kmo. esetén');
-        $blc->note('4. kmo. esetén \`W_(eff)\` hatékony keresztmetszet rugalmas keresztmetszeti modulussal kell számolni.');
+        $blc->def('McRdel', \H3::n2($ec->McRd($f3->_Wel, $f3->_fy)), 'M_(c,Rd,el) = (W_(el)*f_y)/gamma_(M0) = %% [kNm]', '3 . kmo. esetén');
+        $blc->note('4. kmo. esetén $W_(eff)$ hatékony keresztmetszet rugalmas keresztmetszeti modulussal kell számolni.');
         $blc->note('Nyomott zónában a nem oválfuratos lyukgyengítést nem kell figyelembe venni. Húzott öv lyukgyengítésére vizsgálat szükséges:');
-        $blc->txt('Húzott öv lyukgyengítése elhagyható, ha az alábbi feltétel teljesül.', '\`A_(f,n\et)\` az öv nettó keresztmetszeti területe, \`A_f\` az öv bruttó keresztmetszeti területe.');
-        $blc->math('(A_(f,n\et)*0.9*f_u)/gamma_(M2) >= (A_f*f_y)/gamma_(M0)', '');
-        $blc->input('MEd', '`M_(Ed)` hajlítónyomaték', 100, 'kNm');
-        $blc->label($f3->_MEd/$f3->_McRdpl, 'Hajlítási kihasználtság');
+        $blc->txt('Húzott öv lyukgyengítése elhagyható, ha az alábbi feltétel teljesül:', '$A_(f,n et)$ az öv nettó keresztmetszeti területe, $A_f$ az öv bruttó keresztmetszeti területe.');
+        $blc->math('(A_(f,n et)*0.9*f_u)/gamma_(M2) >= (A_f*f_y)/gamma_(M0)', '');
+        $blc->label($f3->_MEd/$f3->_McRdpl, 'Képlékeny hajlítási kihasználtság');
+        $blc->txt('', '$M_(Ed)/M_(c,Rd,pl)$');
+        $blc->note('3 kmo. esetén:');
+        $blc->label($f3->_MEd/$f3->_McRdel, 'Rugalmas hajlítási kihasználtság');
+        $blc->txt('', '$M_(Ed)/M_(c,Rd,el)$');
 
         $blc->h1('Hajlítás és nyírás kölcsönhatása');
         if ($f3->_VEd >= 0.5*$f3->_VplRd) {
-            $blc->txt('Kölcsönhatás figyelembe veendő!');
-            $blc->math('v_(Ed) = '.$f3->_VEd.'[kN] %%% >= %%% 0.5*V_(c,Rd) = '. 0.5*$f3->_VplRd.' [kN]');
-            $blc->def('rho', pow(((2*$f3->_VEd)/$f3->_VplRd - 1), 2), 'rho = ((2*V_(Ed))/V_(pl,Rd) - 1)^2 = %%');
-            $blc->note('Nyomatéki teherbírás számításakor a folyáshatárt a nyírt területen `(1 - rho)` csökkentő tényezővel kell figyelembe venni.');
+            $blc->math('v_(Ed) = '.$f3->_VEd.'[kN] %%% >= %%% 0.5*V_(c,Rd) = '. \H3::n2(0.5*$f3->_VplRd).' [kN]');
+            $blc->txt('Ezért a kölcsönhatás figyelembe veendő!');
+            $blc->def('rho', \H3::n4(pow(((2*$f3->_VEd)/$f3->_VplRd - 1), 2)), 'rho = ((2*V_(Ed))/V_(pl,Rd) - 1)^2 = %%');
+            $blc->note('Nyomatéki teherbírás számításakor a folyáshatárt a nyírt területen $(1 - rho)$ csökkentő tényezővel kell figyelembe venni.');
             $blc->txt('1 és 2. kmo. nagy tengely körül hajlított I szelvények esetében a nyomatéki teherbírás:');
             $blc->math('A_w = A_v = '.$f3->_A_v.' [mm^2] %%% t_w = t = '.$f3->_t.' [mm]', 'Gerinc keresztmetszeti terület és gerincvastagság értékadása.');
-            $blc->def('MyVRd', ($f3->_Wpl - ($f3->_rho*pow($f3->_A_v, 2))/(4*$f3->_t))*($f3->_fy/($f3->__GM0*1000000)), 'M_(y,V,Rd) = (W_(pl,y) - (rho*A_w^2)/(4*t_w))*f_y/gamma_(M0) = %% [kNm]', '');
-            $blc->txt('', 'De: \`M_(y,V,Rd) < M_(c,Rd)\`');
+            $blc->def('MyVRd', \H3::n2(($f3->_Wpl - ($f3->_rho*pow($f3->_A_v, 2))/(4*$f3->_t))*($f3->_fy/($f3->__GM0*1000000))), 'M_(y,V,Rd) = (W_(pl,y) - (rho*A_w^2)/(4*t_w))*f_y/gamma_(M0) = %% [kNm]', '');
+            $blc->txt('', 'De: $M_(y,V,Rd) < M_(c,Rd)$');
             $blc->note('Rugalmas számítás során a 3. és 4. kmo. szelvényekre a kölcsönhatást a feszültség alapú általános formulával kell meghatározni.');
         } else {
             $blc->label('yes', 'Kölcsönhatás vizsgálata nem szükséges.');
@@ -108,14 +120,13 @@ Class SteelSection extends \Ecc
         $blc->def('NplRd', $ec->NplRd($f3->_A, $f3->_mat, $f3->_t), 'N_(pl,Rd) = (A*f_y)/gamma_(M,0) = %% [kN]', 'Teljes km. folyási ellenállása');
         $blc->def('NuRd', $ec->NuRd($f3->_A_net, $f3->_mat, $f3->_t), 'N_(u,Rd) = (0.9*A_(n\et)*f_u)/gamma_(M,2) = %% [kN]', 'Nettó km. képlékeny töréssel szembeni ellenállása');
         $blc->success0('NtRd');
-        $blc->def('NtRd', $ec->NtRd($f3->_A, $f3->_A_net, $f3->_mat, $f3->_t), 'N_(t,Rd) = min(N_(pl,Rd), N_(u,Rd)) = %% [kN]', 'Húzási ellenállás');
+            $blc->def('NtRd', $ec->NtRd($f3->_A, $f3->_A_net, $f3->_mat, $f3->_t), 'N_(t,Rd) = min(N_(pl,Rd), N_(u,Rd)) = %% [kN]', 'Húzási ellenállás');
         $blc->success1('NtRd');
-
-        $blc->input('NEd', '`N_(Ed)` húzóerő', 200, 'kN');
         $blc->label($f3->_NEd/$f3->_NtRd, 'Húzási kihasználtság');
+        $blc->txt('', '$N_(Ed)/N_(t,Rd)$');
 
         $blc->h1('Központosan nyomott rudak ellenállása');
-        $blc->note('1, 2, 3. kmo. szelvényekre. `A = A_(eff)` 4. kmo.-ra.');
+        $blc->note('1, 2, 3. kmo. szelvényekre. $A = A_(eff)$ 4. kmo.-ra.');
         $blc->success0('NcRd');
             $blc->def('NcRd', $ec->NcRd($f3->_A, $f3->_fy), 'N_(c,Rd) = %% [kN]');
         $blc->success1('NcRd');
@@ -139,9 +150,9 @@ módszer segíthet, mert a gerinc besorolása összetett igénybevételre kedvez
 osztályú szelvény rugalmas ellenőrzése során a feszültségeket egyébként is kiszámoljuk, tehát a második módszer használata
 sem okoz külön nehézséget, ezt javasoljuk. A 4. osztályú szelvények vizsgálatához való (5.34) összefüggésben pedig az EC3
 előírja az első módszer használatát.');
-        $blc->txt('Az alábbi feltételek teljesülése esetén 1 és 2. kmo. övlemezes szelvények esetén a normálerő nem csökkenti az y tengely körüli nyomatéki ellenállást:');
+        $blc->txt('Az alábbi feltételek teljesülése esetén 1 és 2. kmo. övlemezes szelvények esetén a normálerő nem csökkenti az *y* tengely körüli nyomatéki ellenállást:');
         $blc->math('N_(Ed) = '.$f3->_NEd.'[kN] %%% < %%% 0.25*N_(pl,Rd) = '. 0.25*$f3->_NplRd.' [kN]');
         $blc->math('N_(Ed) = '.$f3->_NEd.'[kN] %%% < %%% (0.5*h_w*t_w*f_y)/gamma_(M0) = ');
-        $blc->md('`TODO` h_w miatt nem ellenőrizhető');
+        $blc->md('`TODO h_w miatt nem ellenőrizhető`');
     }
 }
