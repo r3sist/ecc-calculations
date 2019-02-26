1050f7c420bbda33497744e771348feebdfc55e8
resist <vankos@resist.hu>
2019-02-26 10:15:29 +0100

    Added notes for Imeprfection

--- Calculations/Column.php
+++ Calculations/Column.php
@@ -128,18 +128,20 @@ Class Column extends \Ecc
 
         // =============================================================================================================
         $blc->h1('Imperfekció', 'Mértékadó normálerőkből számított imperfekciós vízszintes terhelés');
-
+        $blc->note('*[Vasbeton szerkezetek (2016) 5.2.]*');
         $blc->note('Tartószerkezet geometriai pontatlanságának figyelembe vétele a merevítőrendszert terheli.');
         $blc->note('+x +y -x -y irányokban is hathat.');
         $blc->numeric('Phi0', ['Phi_0', 'Elfordulás alapértéke'], 0.005, '', 'Alapértelmezetten 1/200');
         $blc->numeric('m', ['m', 'Egy szinten lévő pillérek száma'], 10, 'db', '');
         $blc->def('alpham', sqrt(0.5*(1 + 1/$f3->_m)), 'alpha_m = sqrt(0.5*(1+1/m)) = %%', 'Csökkentő tényező oszlopszámtól függően');
         $blc->def('alphan', min(1, max(0.6667, 2/sqrt($f3->_l))), 'alpha_n = min{(1),( max{(2/sqrt(l) = '. 2/sqrt($f3->_l).'),(0.667):}):} = %%', '');
+        $blc->note('$l$ az épület teljes magassága');
         $blc->def('Phi', $f3->_alpham*$f3->_alphan*$f3->_Phi0, 'Phi = alpha_m*alpha_n*Phi_0 = %%', 'Elfordulás oszlopszámtól függő értéke');
 //        $blc->lst('upsilon', ['Merevített szerkezet (1)' => 1, 'Kilendülő szerkezet (2)' => 2], ['upsilon', 'Oszlop kihajlási fél-hullámhossza'], 1);
         $blc->def('l0', $f3->_l*$f3->_upsilon, 'l_0 = upsilon*l = %% [m]', 'Kihajlási hossz');
         $blc->def('e', \H3::n1($f3->_l0*1000*$f3->_Phi), 'e_(calc) = l_0*Phi = %% [mm] = l/'.\H3::n0(($f3->_l*1000)/\H3::n1($f3->_l0*1000*$f3->_Phi)), 'Egyenértékű elmozdulás');
         $blc->note('Egyszerűsítésként $l/400$ használatát engedi a szabvány');
+        $blc->note('A vsz. többleterők eredője lehet szintenként egy eltolóerő, vagy ± irányból származó csavarónyomaték');
 
         $fields = [
             ['name' => 'name', 'title' => 'Pillér csoport neve', 'type' => 'input', 'sum' => false],
