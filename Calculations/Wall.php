922a60e3cf479025056a10835409a47289e62837
resist <vankos@resist.hu>
2019-06-05 15:24:25 +0200

    Wall: fix min/max ASCIIMath  expressions

--- Calculations/Wall.php
+++ Calculations/Wall.php
@@ -44,8 +44,8 @@ Class Wall extends \Ecc
                 }
                 $blc->def('A_sminv', $f3->_A_c*0.003, 'A_(s,min,v) = A_c*0.3% = %% [(mm^2)/m]', 'Minimális függőleges vasmennyiség');
                 $blc->def('A_smaxv', $f3->_A_c*0.04, 'A_(s,max,v) = A_c*4% = %% [(mm^2)/m]', 'Maximális függőleges vasmennyiség');
-                $blc->def('s_maxv', min($f3->_t_w*3, 400), 's_(max,v) = min(3*t_w, 400) = %% [mm]', 'Maximális függőleges vastávolság');
-                $blc->def('A_sminh', min($f3->_A_sminv*0.25, $f3->_A_c*0.0015), 'A_(s,min,h) = min(A_(s,min,v)*25%, A_c*0.15%) = %% [(mm^2)/m]', 'Minimális vízszintes vasmennyiség');
+                $blc->def('s_maxv', min($f3->_t_w*3, 400), 's_(max,v) = min{(3*t_w), (400):} = %% [mm]', 'Maximális függőleges vastávolság');
+                $blc->def('A_sminh', min($f3->_A_sminv*0.25, $f3->_A_c*0.0015), 'A_(s,min,h) = min{(A_(s,min,v)*25%), (A_c*0.15%):} = %% [(mm^2)/m]', 'Minimális vízszintes vasmennyiség');
                 $blc->def('A_smaxh', $f3->_A_c*0.04, 'A_(s,max,h) = A_c*4% = %% [(mm^2)/m]', 'Maximális vízszintes vasmennyiség');
                 $blc->def('s_maxh', 400, 's_(max,h) = %% [mm]', 'Maximális vízszintes vastávolság');
                 $blc->txt('', 'Két- vagy többrétegű vasalás esetén összekötő vasalás alkalmazandó legalább $4 [(db)/m^2]$ és minimum $150 [(mm^2)/m^2]$ összesített keresztmetszettel.');
@@ -56,9 +56,9 @@ Class Wall extends \Ecc
                 } else {
                     $blc->txt('$b/h = '.number_format($f3->_b/$f3->_h, 1).' > 2 $: Faltartóba 1 rétegű vasalás elég');
                 }
-                $blc->def('A_sminvh', max($f3->_A_c*0.001, 150), 'A_(s,min,vh) = max(150, A_c*0.1%) = %% [(mm^2) / m]', 'Minimális vízszintes és függőleges vasmennyiség');
+                $blc->def('A_sminvh', max($f3->_A_c*0.001, 150), 'A_(s,min,vh) = max{(150), (A_c*0.1%):} = %% [(mm^2) / m]', 'Minimális vízszintes és függőleges vasmennyiség');
                 $blc->def('A_smaxvh', $f3->_A_c*0.04, 'A_(s,max,vh) = A_c*4% = %% [(mm^2) / m]', 'Maximális vízszintes és függőleges vasmennyiség');
-                $blc->def('s_maxvh', min(3*$f3->_t_w, 300), 's_(max,vh) = min(2*t_w, 300) = %% [mm]', 'Maximális vízszintes és függőleges vastávolság');
+                $blc->def('s_maxvh', min(3*$f3->_t_w, 300), 's_(max,vh) = min{(2*t_w), (300):} = %% [mm]', 'Maximális vízszintes és függőleges vastávolság');
                 $blc->txt('', 'Ha faltartó valamely szakaszán a függőleges vasalás mennyisége $A_s > 0.02A_c$, akkor ott kengyelekből álló kereszt irányú vasalást kell elhelyezni az oszlopokra vonatkozó követelmények használatával.');
                 break;
         }
