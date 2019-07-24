ddc0a8caa34a5ef668f3c5f961bbff8c1fe3a871
resist <vankos@resist.hu>
2019-07-24 09:28:31 +0200

    Weld: refactor main module

--- Calculations/Weld.php
+++ Calculations/Weld.php
@@ -12,7 +12,7 @@ Class Weld extends \Ecc
      * @var $ec \Ec\Ec
      * @throws \Exception
      */
-    public function moduleWeld(object $f3, object $blc, object $ec) {
+    public function moduleWeldOld(object $f3, object $blc, object $ec) {
 
         $blc->region0('r0', 'Varrat számítások');
         $blc->def('w', $f3->_w + 1, 'w_(sarok) = %%');
@@ -44,6 +44,46 @@ Class Weld extends \Ecc
         $blc->success1('s0');
     }
 
+    /**
+     *
+     * @var $f3 \Base
+     * @var $blc \Blc
+     * @var $ec \Ec\Ec
+     * @throws \Exception
+     */
+    public function moduleWeld(int $length, int $a, float $F, string $steelMaterialName = 'S235', int $tPlate = 10, $weldOnBothSide = false) {
+        $f3 = \Base::instance();
+        $blc = \Blc::instance();
+        $ec = \Ec\Ec::instance();
+
+        $blc->region0('r0', 'Varrat számítások');
+            $blc->def('w', \V3::numeric($weldOnBothSide) + 1, 'w_(sarok) = %%');
+            $blc->def('l', $length - 2 * $a, 'l = L - 2*a = %% [mm]', 'Figyelembe vett varrathossz');
+            $blc->def('bw', $ec->matProp($steelMaterialName, 'betaw'), 'beta_w = %%', 'Hegesztési tényező');
+            $blc->def('fy', $ec->fy($steelMaterialName, $tPlate), 'f_y=%% [N/(mm^2)]', 'Folyáshatár');
+            $blc->def('fu', $ec->fu($steelMaterialName, $tPlate), 'f_u=%% [N/(mm^2)]', 'Szakítószilárdság');
+            $blc->math('F_(Ed) = '.$F.'[kN]');
+            $blc->def('FwEd', $F / $f3->_l * 1000, 'F_(w,Ed) = F_(Ed)/l = %% [(kN)/m]', 'Fajlagos igénybevétel');
+        $blc->region1('r0');
+
+        if ($f3->_l <= 30) {
+            $blc->danger('Varrathossz rövidebb $30 [mm]$-nél! Szabvány szerint nem figyelembe vehető.');
+        }
+
+        if ($f3->_l <= 6 * $a) {
+            $blc->danger('Varrathossz rövidebb $6*a = ' . 6 * $a . '[mm]$-nél! Szabvány szerint nem figyelembe vehető.');
+        }
+
+        if (150 * $a < $f3->_l) {
+            $blc->danger('$l >= 150*a = ' . 150 * $a . ' [mm]$, ezért indokolt a varrat teherbírásának csökkentése, nem zártszelvények esetén.');
+        }
+
+        $blc->def('FwRd', \H3::n2(($f3->_fu * $a) / (sqrt(3) * $f3->_bw * $f3->__GM2) * ($f3->_w)), 'F_(w,Rd) = (f_u*a)/(sqrt(3)*beta_w*gamma_(M2))*w_(sarok)= %% [(kN)/m]', 'Fajlagos teherbírás');
+        $blc->def('FwRdS', \H3::n2(($f3->_FwRd * $f3->_l / 1000)), 'F_(w,Rd,sum) = F_(w,Rd)*l = %% [kN]', 'Varratkép teljes teherbírása:');
+        $blc->label($F / $f3->_FwRdS, 'Kihasználtság');
+        $blc->txt('', '$(F = '.$F.'[kN])/F_(w,Rd,sum)$');
+    }
+
     public function calc(object $f3, object $blc, object $ec): void
     {
         $blc->numeric('a', ['a', 'Varrat gyökméret'], 4, 'mm', '');
@@ -53,7 +93,7 @@ Class Weld extends \Ecc
         $blc->numeric('F', ['F', 'Erő'], 10, 'kN');
         $blc->boo('w', 'Kétoldali sarokvarrat', 0);
 
-        $this->moduleWeld($f3, $blc, $ec);
+        $this->moduleWeldOld($f3, $blc, $ec);
 
         $blc->hr();
         $blc->img('https://structure.hu/ecc/weld0.jpg', 'CÉH Tekla hegesztési utasítás');
