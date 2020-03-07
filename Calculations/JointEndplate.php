d437057d3169dfa29d1d6daf437ccc073aacc5a2
r3sist <r3sist@users.noreply.github.com>
2020-03-07 15:26:29 +0100

    Remove U3 from composer

--- Calculations/JointEndplate.php
+++ Calculations/JointEndplate.php
@@ -22,7 +22,25 @@ Class JointEndplate
 
     public function calc(Base $f3, Blc $blc, Ec $ec): void
     {
-        $ec->matList('steelMaterialName', 'S235', ['', 'Szelvények, lemezek anyagminősége']);
+        $blc->note('A nyomóerő a keresztmetszeti ellenállás 5%-át nem haladhatja meg!');
+        
+        $ec->matList('steelMaterialName', 'S235', ['', 'Szelvények, lemezek anyagminősége'], 'steel');
         $ec->spreadMaterialData($f3->_steelMaterialName, 's');
+
+        $ec->matList('boltMaterialName', '8.8', ['', 'Csavarok anyagminősége'], 'bolt');
+        $ec->spreadMaterialData($f3->_boltMaterialName, 'b');
+        $ec->boltList('boltName', 'M12', 'Csavar átmérő választása');
+        $blc->def('As', $ec->boltProp($f3->_boltName, 'As'), 'A_s = %% [mm^2]', 'Csavar húzási keresztmetszet');
+        $blc->def('dm', $ec->boltProp($f3->_boltName, 'dm'), 'd_m = %% [mm]', 'Kigombolódási átmérő');
+
+        $blc->def('FtRd', $ec->FtRd($f3->_boltName, $f3->_boltMaterialName, true), 'F_(t,Rd) = %% [kN]', 'Egy csavar húzási ellenállása');
+        $blc->info0();
+            $blc->def('tpminm', ceil(0.175*$ec->boltProp($f3->_boltName, 'd')*($f3->_bfu/$f3->_sfy)), 't_(p,min,m) ge 0.175*d*(f_(ub)/f_y) = %% [mm]', 'Homloklemezek javasolt vastagsága kigombolódás kizáráshoz');
+        $blc->info1();
+
+        $blc->numeric('etaj1', ['eta_(j,1) = M_(Ed)/M_(pl,Rd)', 'Kapcsolódó elemek nyomatéki kihasználtsága'], 0.9);
+
+        
+
     }
 }
